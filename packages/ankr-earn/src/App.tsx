ca74506e6dff4c071e54bf9d59e3314272166d84
Anton Panov <aliengrey88@gmail.com>
2021-12-08 16:22:34 +0300

    [STAKAN-402] Configure store and translations module (#148)
    * build: specify baseUrl in tsconfig
    
    * feat: add base store and translations support

--- packages/ankr-earn/src/App.tsx
+++ packages/ankr-earn/src/App.tsx
@@ -1,13 +1,23 @@
+import { ScrollToTop } from 'modules/common/components/ScrollToTop';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import { AppBase } from './modules/layout/components/AppBase/AppBase';
 import { Routes } from './Routes';
+import { persistor, store } from './store';
+import { Spinner } from './uiKit/Spinner';
 
 function App() {
   return (
     <Router>
-      <AppBase>
-        <Routes />
-      </AppBase>
+      <Provider store={store}>
+        <PersistGate loading={<Spinner />} persistor={persistor}>
+          <AppBase>
+            <ScrollToTop />
+            <Routes />
+          </AppBase>
+        </PersistGate>
+      </Provider>
     </Router>
   );
 }
---
+++ packages/ankr-earn/src/modules/common/const.ts
@@ -0,0 +1,5 @@
+import { Env } from './types';
+
+export const currentEnv: Env = process.env.REACT_APP_API_ENV
+  ? (process.env.REACT_APP_API_ENV as Env)
+  : Env.Stage;
