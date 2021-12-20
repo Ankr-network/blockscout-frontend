86fa40a8c878acf0c673f49d8886d83571456d89
Anton <aliengrey88@gmail.com>
2021-12-20 19:14:27 +0300

    fix: add missed key props

--- packages/ankr-earn/src/modules/common/components/Faq/Faq.tsx
+++ packages/ankr-earn/src/modules/common/components/Faq/Faq.tsx
@@ -5,9 +5,10 @@ import {
   Paper,
   Typography,
 } from '@material-ui/core';
-import { t } from 'modules/i18n/utils/intl';
-import { useFaqStyles as useStyles } from './useFaqStyles';
 import { ReactComponent as AngleDownIcon } from 'assets/img/angle-down-icon.svg';
+import { t } from 'modules/i18n/utils/intl';
+import { uid } from 'react-uid';
+import { useFaqStyles as useStyles } from './useFaqStyles';
 
 export interface IFaq {
   data: Record<string, string>[];
@@ -16,8 +17,8 @@ export interface IFaq {
 export const Faq = ({ data }: IFaq) => {
   const classes = useStyles();
 
-  const FaqList = data.map(el => (
-    <Accordion>
+  const FaqList = data.map((el, i) => (
+    <Accordion key={uid(i)}>
       <AccordionSummary
         expandIcon={<AngleDownIcon />}
         aria-controls={`${el.question}-content`}
