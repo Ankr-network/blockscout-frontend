e6bb699552d122282dae267d88cf00b170e16465
Anton Panov <aliengrey88@gmail.com>
2021-12-23 12:52:57 +0300

    [NA] Fix react overlay (#181)
    * Revert "Revert "[NA] Fix react overlay (#178)" (#180)"
    
    This reverts commit cad631daf3c4081f00c862c524a4d2719a58be23.
    
    * fix: unify DevOverlayFix component

--- packages/ankr-earn/src/App.tsx
+++ packages/ankr-earn/src/App.tsx
@@ -1,7 +1,10 @@
 import { ScrollToTop } from 'modules/common/components/ScrollToTop';
+import { currentEnv } from 'modules/common/const';
+import { Env } from 'modules/common/types';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
+import { DevOverlayFix } from 'ui';
 import { AppBase } from './modules/layout/components/AppBase/AppBase';
 import { Routes } from './Routes';
 import { persistor, store } from './store';
@@ -13,6 +16,8 @@ function App() {
       <Provider store={store}>
         <PersistGate loading={<Spinner />} persistor={persistor}>
           <AppBase>
+            {/* todo: remove when the issue will be resolved */}
+            {currentEnv !== Env.Production && <DevOverlayFix />}
             <ScrollToTop />
             <Routes />
           </AppBase>
