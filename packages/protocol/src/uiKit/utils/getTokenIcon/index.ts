bdf0f3946061cf0904d5b72c02e18bd086e82621
evgeniy-bond <evgeniy.bondarev.01@gmail.com>
2022-01-13 13:11:26 +0400

    [MU-267, MU-268] Add icons (#198)
    * [MU-268] Edit icons logic
    
    * [MU-268] Add icons
    
    * [MU-268] Update postbuild

--- packages/protocol/src/uiKit/utils/getTokenIcon/index.ts
+++ packages/protocol/src/uiKit/utils/getTokenIcon/index.ts
@@ -7,19 +7,24 @@ import stakeIcon from './stake.svg';
 import arbitrumIcon from './arbitrum.svg';
 import defaultIcon from './default-icon.svg';
 import celoIcon from './celo.svg';
-import { Chain } from '@ankr.com/multirpc';
+import nearIcon from './near.svg';
+import bscIcon from './bsc.svg';
+import iotexIcon from './iotex.svg';
 
-const chainIcons: { [key in Chain]: string } = {
-  [Chain.avalanche]: avaxIcon,
-  [Chain.eth]: ethIcon,
-  [Chain.fantom]: ftmIcon,
-  [Chain.polygon]: ksmIcon,
-  [Chain.solana]: solIcon,
-  [Chain.xdai]: stakeIcon,
-  [Chain.arbitrum]: arbitrumIcon,
-  [Chain.celo]: celoIcon,
+const chainIcons = {
+  avalanche: avaxIcon,
+  eth: ethIcon,
+  fantom: ftmIcon,
+  polygon: ksmIcon,
+  solana: solIcon,
+  xdai: stakeIcon,
+  arbitrum: arbitrumIcon,
+  celo: celoIcon,
+  near: nearIcon,
+  bsc: bscIcon,
+  iotex: iotexIcon,
 };
 
-export function getChainIcon(name: Chain) {
-  return chainIcons[name] || defaultIcon;
+export function getChainIcon(name: string) {
+  return chainIcons[name as keyof typeof chainIcons] || defaultIcon;
 }
