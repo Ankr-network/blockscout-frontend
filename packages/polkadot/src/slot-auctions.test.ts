fe6e4161d830207c8c77a022ee9fd65c0a1ffdd0
Victor <91053067+fox-on-rocket@users.noreply.github.com>
2022-01-14 07:37:26 +0100

    [STAKAN-464] Added separate unit-tests

--- packages/polkadot/src/slot-auctions.test.ts
+++ packages/polkadot/src/slot-auctions.test.ts
@@ -1,7 +1,5 @@
-/* eslint-disable no-console */
 import BigNumber from 'bignumber.js';
 import { SlotAuctionSdk } from './slot-auction';
-import { ContractManager } from './contract';
 
 class TestSlotAuctionSdk extends SlotAuctionSdk {
   static createRemarkPayload(
@@ -12,35 +10,9 @@ class TestSlotAuctionSdk extends SlotAuctionSdk {
   }
 }
 
-class TestContractManager extends ContractManager {
-  static extractAddressIntoHex(address: string): string {
-    return ContractManager.extractAddressIntoHex(address);
-  }
-}
-
 describe('Test: SlotAuctionSdk', (): void => {
   describe('Case: onchain claim', (): void => {
-    it('Ethereum and ss58 addresses extracted properly', () => {
-      expect(
-        TestContractManager.extractAddressIntoHex(
-          '0x2aD7e656c24A96BFA02AfAFadc87c428df78f603',
-        ),
-      ).toStrictEqual('2ad7e656c24a96bfa02afafadc87c428df78f603');
-      expect(
-        TestContractManager.extractAddressIntoHex(
-          '2aD7e656c24A96BFA02AfAFadc87c428df78f603',
-        ),
-      ).toStrictEqual('2ad7e656c24a96bfa02afafadc87c428df78f603');
-      expect(
-        TestContractManager.extractAddressIntoHex(
-          '5Ei1Lrqyt9mkDnSsW2zD6jU3HKiLbdaooKjv8av4KGFseH97',
-        ),
-      ).toStrictEqual(
-        '74eb6902449cf185aaaef799c0dba20ddf3be3a6e2f4592d5bb6cea33d0e3833',
-      );
-    });
-
-    it('Remark payload composed correctly', () => {
+    it('Remark payload composed correctly', (): void => {
       const payload: Buffer = Buffer.from(
         TestSlotAuctionSdk.createRemarkPayload(
           '0x527e4403255053669cE5f5C1124480fe46138de2',
