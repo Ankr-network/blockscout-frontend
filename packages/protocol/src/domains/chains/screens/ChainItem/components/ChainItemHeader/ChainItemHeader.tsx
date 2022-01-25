5cd21467482db570b029e66b9d5f26a3cc36a5d7
evgeniy-bond <evgeniy.bondarev.01@gmail.com>
2022-01-25 17:38:52 +0300

    [PROTOCOL-296] Refactor maininfo

--- packages/protocol/src/domains/chains/screens/ChainItem/components/ChainItemHeader/ChainItemHeader.tsx
+++ packages/protocol/src/domains/chains/screens/ChainItem/components/ChainItemHeader/ChainItemHeader.tsx
@@ -1,25 +1,22 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { Button, Typography } from '@material-ui/core';
 import { Link as RouterLink } from 'react-router-dom';
 import classNames from 'classnames';
 import { INodeEntity } from '@ankr.com/multirpc';
 
 import { Preloader } from 'uiKit/Preloader';
-import { CopyToClipButton } from 'uiKit/CopyToClipButton';
 import { ArrowRightIcon } from 'uiKit/Icons/ArrowRightIcon';
 import { CopyToClipIcon } from 'uiKit/CopyToClipIcon';
 import { TooltipWrapper } from 'uiKit/TooltipWrapper/TooltipWrapper';
-import { ChainRequestsLabel } from 'domains/chains/screens/Chains/components/ChainRequestsLabel';
 import { fetchChain } from 'domains/chains/actions/fetchChain';
 import { formatChains } from 'domains/chains/screens/Chains/components/ChainsList/ChainsListUtils';
-import { ChainMainInfo } from 'modules/common/components/ChainMainInfo';
 import { t, tHTML } from 'modules/i18n/utils/intl';
 import { ResponseData } from 'modules/api/utils/ResponseData';
-import { useIsMDDown } from 'ui';
 import { AddNetworkButton } from 'modules/auth/components/AddNetwork';
 import { PrivateHeader } from './PrivateHeader';
 import { PlanRoutesConfig } from '../../../../../plan/Routes';
 import { useStyles } from './ChainItemHeaderStyles';
+import { MainInfo } from './MainInfo';
 
 interface ChainItemHeaderProps {
   chain: ResponseData<typeof fetchChain>['chain'];
@@ -41,53 +38,26 @@ export const ChainItemHeader = ({
   loading,
 }: ChainItemHeaderProps) => {
   const classes = useStyles();
-  const isMobile = useIsMDDown();
+
   const [formattedChain] = formatChains([chain]);
   const { rpcLinks, name } = formattedChain;
-
-  const hasArchiveNodes = useMemo(
-    () => nodes?.some(item => item.isArchive),
-    [nodes],
-  );
 
   return (
     <div className={classNames(classes.root, className)}>
       <div className={classes.top}>
         <div className={classes.left}>
-          <ChainMainInfo
-            logoSrc={icon}
+          <MainInfo
             name={name}
-            description={
-              <div className={classes.description}>
-                <ChainRequestsLabel
-                  description={name}
-                  descriptionColor="textSecondary"
-                />
-                {hasCredentials && hasArchiveNodes && (
-                  <TooltipWrapper
-                    hasIcon={false}
-                    tooltipText={tHTML(
-                      'chain-item.header.archived-node-tooltip-text',
-                    )}
-                  >
-                    <Typography
-                      className={classes.archived}
-                      variant="caption"
-                      color="textSecondary"
-                    >
-                      {t('chain-item.header.archived')}
-                    </Typography>
-                  </TooltipWrapper>
-                )}
-              </div>
-            }
+            hasCredentials={hasCredentials}
+            icon={icon}
+            nodes={nodes}
           />
           <AddNetworkButton chain={formattedChain} hasPlusIcon />
         </div>
 
         <div className={classes.right}>
           {rpcLinks.map(link => {
-            return isMobile ? (
+            return (
               <React.Fragment key={link}>
                 <Typography
                   variant="body2"
@@ -99,19 +69,12 @@ export const ChainItemHeader = ({
                 <CopyToClipIcon
                   text={link}
                   message={t('common.copy-message')}
+                  copyText={t('common.copy-text')}
                   size="l"
                   textColor="textPrimary"
                   className={classes.copyToClip}
                 />
               </React.Fragment>
-            ) : (
-              <CopyToClipButton
-                text={link}
-                key={link}
-                textMessage={t('common.copy-message')}
-                buttonText={t('common.copy-text')}
-                className={classes.copyToClip}
-              />
             );
           })}
         </div>
