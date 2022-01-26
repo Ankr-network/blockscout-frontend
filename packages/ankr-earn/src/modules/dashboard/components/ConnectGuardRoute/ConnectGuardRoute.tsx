e899fd83f977a9684a00fdc3cf93a2ff36de1201
Anton P <aliengrey88@gmail.com>
2022-01-26 12:20:58 +0300

    refactor: add proper container for the connect guard route

--- packages/ankr-earn/src/modules/dashboard/components/ConnectGuardRoute/ConnectGuardRoute.tsx
+++ packages/ankr-earn/src/modules/dashboard/components/ConnectGuardRoute/ConnectGuardRoute.tsx
@@ -1,9 +1,13 @@
-import { Box, Button, Container } from '@material-ui/core';
+import { Box, Button } from '@material-ui/core';
 import { useAuth } from 'modules/auth/hooks/useAuth';
 import { t } from 'modules/i18n/utils/intl';
 import { DefaultLayout } from 'modules/layout/components/DefautLayout';
-import { MATIC_STAKING_NETWORKS, POLYGON_PROVIDER_ID } from 'modules/stake-polygon/const';
+import {
+  MATIC_STAKING_NETWORKS,
+  POLYGON_PROVIDER_ID,
+} from 'modules/stake-polygon/const';
 import { Route, RouteProps } from 'react-router';
+import { Container } from 'uiKit/Container';
 import { Placeholder } from '../Placeholder';
 
 interface IConnectGuardRouteProps extends RouteProps {}
@@ -11,7 +15,10 @@ interface IConnectGuardRouteProps extends RouteProps {}
 export const ConnectGuardRoute = ({
   ...routeProps
 }: IConnectGuardRouteProps) => {
-  const { dispatchConnect, isConnected } = useAuth(POLYGON_PROVIDER_ID, MATIC_STAKING_NETWORKS);
+  const { dispatchConnect, isConnected } = useAuth(
+    POLYGON_PROVIDER_ID,
+    MATIC_STAKING_NETWORKS,
+  );
 
   if (isConnected) {
     return <Route {...routeProps} />;
