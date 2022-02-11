1a701a0dda6ac1c4e9aa5a528f5c4aaeb93fe00f
Anton Panov <aliengrey88@gmail.com>
2022-02-11 16:46:08 +0300

    fix: wrong address requests

--- packages/ankr-earn/src/modules/dashboard/screens/Dashboard/hooks/useDashboard.ts
+++ packages/ankr-earn/src/modules/dashboard/screens/Dashboard/hooks/useDashboard.ts
@@ -1,6 +1,11 @@
 import { useDispatchRequest } from '@redux-requests/react';
 import { useConnectedData } from 'modules/auth/hooks/useConnectedData';
 import { useProviderEffect } from 'modules/auth/hooks/useProviderEffect';
+import {
+  BSC_NETWORK_BY_ENV,
+  ETH_NETWORK_BY_ENV,
+  FTM_NETWORK_BY_ENV,
+} from 'modules/common/const';
 import { getEth2SwapData } from 'modules/eth2Swap/actions/getEth2SwapData';
 import { fetchStats as fetchBNBStats } from 'modules/stake-bnb/actions/fetchStats';
 import { fetchTxHistory as fetchBNBTxHistory } from 'modules/stake-bnb/actions/fetchTxHistory';
@@ -8,10 +13,7 @@ import { getCommonData as fetchFTMStats } from 'modules/stake-fantom/actions/get
 import { fetchAPY as fetchPolygonAPY } from 'modules/stake-polygon/actions/fetchAPY';
 import { fetchStats as fetchPolygonStats } from 'modules/stake-polygon/actions/fetchStats';
 import { fetchTxHistory as fetchPolygonTxHistory } from 'modules/stake-polygon/actions/fetchTxHistory';
-import {
-  AvailableWriteProviders,
-  BlockchainNetworkId,
-} from 'provider/providerManager/types';
+import { AvailableWriteProviders } from 'provider/providerManager/types';
 
 export const useDashboard = () => {
   const { chainId } = useConnectedData(AvailableWriteProviders.ethCompatible);
@@ -19,8 +21,7 @@ export const useDashboard = () => {
 
   useProviderEffect(() => {
     switch (chainId) {
-      case BlockchainNetworkId.goerli:
-      case BlockchainNetworkId.mainnet:
+      case ETH_NETWORK_BY_ENV:
         dispatchRequest(fetchPolygonStats());
         dispatchRequest(fetchPolygonTxHistory());
         dispatchRequest(fetchPolygonAPY());
@@ -31,14 +32,12 @@ export const useDashboard = () => {
         );
         break;
 
-      case BlockchainNetworkId.smartchain:
-      case BlockchainNetworkId.smartchainTestnet:
+      case BSC_NETWORK_BY_ENV:
         dispatchRequest(fetchBNBStats());
         dispatchRequest(fetchBNBTxHistory());
         break;
 
-      case BlockchainNetworkId.fantom:
-      case BlockchainNetworkId.fantomTestnet:
+      case FTM_NETWORK_BY_ENV:
         dispatchRequest(fetchFTMStats());
         break;
 
