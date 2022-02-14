0092549d785ede723c62571823f993debb0ac15f
Anton Panov <aliengrey88@gmail.com>
2022-02-14 11:59:33 +0300

    improvement: STAKAN-1060 add actual FAQ data (#341)

--- packages/ankr-earn/src/modules/stake-fantom/screens/StakeFantom/hooks/useFaq.ts
+++ packages/ankr-earn/src/modules/stake-fantom/screens/StakeFantom/hooks/useFaq.ts
@@ -1,16 +1,37 @@
+import { useQuery } from '@redux-requests/react';
 import { IFaqItem } from 'modules/common/components/Faq';
 import { useLocaleMemo } from 'modules/i18n/hooks/useLocaleMemo';
-import { t } from 'modules/i18n/utils/intl';
+import { t, tHTML } from 'modules/i18n/utils/intl';
+import { getCommonData } from 'modules/stake-fantom/actions/getCommonData';
+
+const FAQ_ITEMS_COUNT = 9;
+const DEFAULT_MIN_STAKE = 1;
 
 export const useFaq = () => {
+  const { data } = useQuery({
+    type: getCommonData,
+  });
+
+  const minAmount = data?.minStake.toNumber() || DEFAULT_MIN_STAKE;
+
   const faqItems: IFaqItem[] = useLocaleMemo(
-    () => [
-      {
-        question: t('How long after unstaking can I withdraw my FTM?'),
-        answer: t('1 day'),
-      },
-    ],
-    [],
+    () =>
+      [...Array(FAQ_ITEMS_COUNT)].map((_, i) => {
+        const itemIndex = i + 1;
+        let answerParams: undefined | Record<string, string | number>;
+
+        if (itemIndex === 2) {
+          answerParams = {
+            value: minAmount,
+          };
+        }
+
+        return {
+          question: t(`stake-fantom.faq.question-${itemIndex}`),
+          answer: tHTML(`stake-fantom.faq.answer-${itemIndex}`, answerParams),
+        };
+      }),
+    [minAmount],
   );
 
   return faqItems;
