b064c514210079ae7af1fe5a82396fb22f5e831b
Anton <14254374+0xmad@users.noreply.github.com>
2022-02-15 08:05:25 +0300

    fix(dashboard): style fixes for dashboard placeholders (#334)

--- packages/ankr-earn/src/modules/dashboard/components/Placeholder/Placeholder.tsx
+++ packages/ankr-earn/src/modules/dashboard/components/Placeholder/Placeholder.tsx
@@ -1,20 +1,29 @@
 import { Paper, Typography } from '@material-ui/core';
-import React, { ReactNode } from 'react';
-import PortfolioStartStaking from './assets/portfolio-start-staking.png';
+import { ReactNode } from 'react';
+import portfolioStartStaking from './assets/portfolio-start-staking.png';
 import { usePlaceholderStyles } from './usePlaceholderStyles';
 
 interface IPlaceholderProps {
   btnSlot?: ReactNode;
   title: string;
+  src?: string;
 }
 
-export const Placeholder = ({ title, btnSlot }: IPlaceholderProps) => {
+export const Placeholder = ({
+  title,
+  btnSlot,
+  src = '',
+}: IPlaceholderProps) => {
   const classes = usePlaceholderStyles();
 
   return (
     <Paper className={classes.root}>
       <div className={classes.imgWrap}>
-        <img className={classes.img} src={PortfolioStartStaking} alt={title} />
+        <img
+          className={classes.img}
+          src={src || portfolioStartStaking}
+          alt={title}
+        />
       </div>
 
       <Typography className={classes.title}>{title}</Typography>
