ef1fd10ada18bbe1bea7855e48317f85aa818000
Anton <14254374+0xmad@users.noreply.github.com>
2022-02-15 14:48:01 +0300

    feat(header-banner): add ability to close header banner [STAKAN-1053] (#347)
    * Add session service as a wrapper to work with session storage
    * Hide banner only for current user session

--- packages/ankr-earn/src/modules/layout/components/SwitchBanner/useSwitchBannerStyles.ts
+++ packages/ankr-earn/src/modules/layout/components/SwitchBanner/useSwitchBannerStyles.ts
@@ -3,17 +3,18 @@ import { makeStyles } from '@material-ui/core';
 export const useSwitchBannerStyles = makeStyles(theme => ({
   root: {
     display: 'grid',
-    gridTemplateColumns: 'auto 1fr auto',
+    gridTemplateColumns: 'auto 1fr auto 12px',
     gap: theme.spacing(1, 2.5),
     alignItems: 'center',
     justifyContent: 'center',
     paddingTop: theme.spacing(1),
     paddingBottom: theme.spacing(1),
+    position: 'relative',
 
     backgroundColor: theme.palette.background.paper,
 
     [theme.breakpoints.up('sm')]: {
-      gridTemplateColumns: 'repeat(3, auto)',
+      gridTemplateColumns: 'repeat(4, auto)',
     },
   },
 
@@ -40,5 +41,24 @@ export const useSwitchBannerStyles = makeStyles(theme => ({
     fontSize: 16,
     fontWeight: 600,
     borderRadius: 16,
+
+    [theme.breakpoints.down('sm')]: {
+      fontSize: 12,
+      width: 80,
+    },
+  },
+
+  closeWrapper: {
+    border: 'none',
+    background: 'none',
+    color: theme.palette.text.secondary,
+    cursor: 'pointer',
+
+    position: 'absolute',
+    right: theme.spacing(2),
+
+    '& > svg': {
+      marginBottom: -1,
+    },
   },
 }));
