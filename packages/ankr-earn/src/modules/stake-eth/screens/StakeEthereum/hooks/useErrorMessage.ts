1f75c3c078014e74f7451454d9163945b52d7a8e
Anton Panov <aliengrey88@gmail.com>
2022-03-25 19:06:31 +0300

    chore: STAKAN-369 update ETH RPC URL (#598)
    * chore: update ETH RPC URL
    
    * chore: add error handling for the ETH staking page

--- packages/ankr-earn/src/modules/stake-eth/screens/StakeEthereum/hooks/useErrorMessage.ts
+++ packages/ankr-earn/src/modules/stake-eth/screens/StakeEthereum/hooks/useErrorMessage.ts
@@ -1,11 +1,35 @@
+import { useDispatchRequest, useQuery } from '@redux-requests/react';
+
+import { getAPY } from 'modules/stake-eth/actions/getAPY';
+import { getCommonData } from 'modules/stake-eth/actions/getCommonData';
+
 interface IUseErrorMessage {
   hasError: boolean;
   onErroMessageClick: () => void;
 }
 
 export const useErrorMessage = (): IUseErrorMessage => {
-  const hasError = false;
-  const onErroMessageClick = () => {};
+  const dispatchRequest = useDispatchRequest();
+
+  const { error: apyError } = useQuery({
+    type: getAPY,
+  });
+
+  const { error: commonDataError } = useQuery({
+    type: getCommonData,
+  });
+
+  const onErroMessageClick = () => {
+    if (commonDataError) {
+      dispatchRequest(getCommonData());
+    }
+
+    if (apyError) {
+      dispatchRequest(getAPY());
+    }
+  };
+
+  const hasError = !!commonDataError || !!apyError;
 
   return {
     hasError,
