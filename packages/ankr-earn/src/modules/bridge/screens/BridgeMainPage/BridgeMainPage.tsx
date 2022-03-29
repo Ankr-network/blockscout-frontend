a350367ca549fef3b7a6343f03be497dc9868451
vpcodebase <84332922+vpcodebase@users.noreply.github.com>
2022-03-29 12:58:23 +0300

    fix(bridge): incorrect storage of approval data (#498)
    #STAKAN-1186

--- packages/ankr-earn/src/modules/bridge/screens/BridgeMainPage/BridgeMainPage.tsx
+++ packages/ankr-earn/src/modules/bridge/screens/BridgeMainPage/BridgeMainPage.tsx
@@ -1,12 +1,16 @@
 import { Box } from '@material-ui/core';
+import { resetRequests } from '@redux-requests/core';
+import { useEffect } from 'react';
 
 import { AvailableWriteProviders } from 'provider';
 
 import { useConnectedData } from 'modules/auth/hooks/useConnectedData';
+import { approve } from 'modules/bridge/actions/approve';
 import { BridgeContainer } from 'modules/bridge/components/BridgeContainer';
 import { Notification } from 'modules/bridge/components/Notification';
 import { TxView } from 'modules/bridge/screens/BridgeMainPage/components/TxView';
 import { t } from 'modules/i18n/utils/intl';
+import { useAppDispatch } from 'store/useAppDispatch';
 
 import { BridgeMainView } from './components/BridgeMainView';
 import { useTxnData } from './hooks/useTxnData';
@@ -16,10 +20,17 @@ export const BridgeMainPage = (): JSX.Element => {
     AvailableWriteProviders.ethCompatible,
   );
   const txnData = useTxnData();
+  const dispatch = useAppDispatch();
 
   const isTxViewShowed = !!txnData;
   const isMainViewShowed = !isTxViewShowed;
   const isNotificationShowed = isMainViewShowed && isConnected;
+
+  useEffect(() => {
+    return () => {
+      dispatch(resetRequests([approve.toString()]));
+    };
+  }, [dispatch]);
 
   return (
     <Box component="section" py={{ xs: 5, md: 8 }}>
