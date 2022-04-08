41140ae74501a3a11314e2313201880c77fdd77a
evgeniy-bond <evgeniy.bondarev.01@gmail.com>
2022-04-08 18:28:14 +0300

    feat(protocol): MRPC-686 Add layout

--- packages/protocol/src/domains/account/screens/AccountDetails/AccountDetails.tsx
+++ packages/protocol/src/domains/account/screens/AccountDetails/AccountDetails.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
+import { ThemeProvider, Box } from '@material-ui/core';
 
+import { mainTheme } from 'ui';
 import { t } from 'modules/i18n/utils/intl';
 import { useSetBreadcrumbs } from 'modules/layout/components/Breadcrumbs';
 import { AccountRoutesConfig } from 'domains/account/Routes';
+import { useStyles } from './AccountDetailsStyles';
 
 export const AccountDetails = () => {
+  const classes = useStyles();
+
   useSetBreadcrumbs([
     {
       title: t(AccountRoutesConfig.accountDetails.breadcrumbs),
     },
   ]);
 
-  return <>AccountDetails</>;
+  return (
+    <ThemeProvider theme={mainTheme}>
+      <Box className={classes.root}>
+        <Box className={classes.top}>
+          <Box className={classes.balance}>Balance</Box>
+          <Box className={classes.topUp}>Top Up</Box>
+        </Box>
+        <Box className={classes.payments}>Payments</Box>
+        <Box className={classes.chart}>Chart</Box>
+      </Box>
+    </ThemeProvider>
+  );
 };
---
+++ packages/protocol/src/domains/account/screens/AccountDetails/AccountDetailsStyles.ts
@@ -0,0 +1,22 @@
+import { Theme } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { BREAKPOINTS } from 'ui';
+
+export const useStyles = makeStyles<Theme>(theme => ({
+  root: {
+    display: 'flex',
+    [theme.breakpoints.down(BREAKPOINTS.values.WXGAPlus)]: {
+      flexDirection: 'column',
+    },
+    [theme.breakpoints.down('xs')]: {
+      paddingBottom: theme.spacing(3.5),
+    },
+    maxWidth: 940,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  top: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+}));
