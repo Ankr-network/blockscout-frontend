7cc8c4c66fe3dbdbb319672370608e278ffbfe7a
Denise <41983880+Denise54321@users.noreply.github.com>
2022-04-29 11:29:32 +0800

    feat(MRPC): MRPC-736 Update Cross Menu Styles (#793)

--- packages/standalone-protocol/src/domains/chains/screens/ChainItem/components/CrossMenu/CrossMenu.tsx
+++ packages/standalone-protocol/src/domains/chains/screens/ChainItem/components/CrossMenu/CrossMenu.tsx
@@ -1,137 +1,22 @@
-import React, { useMemo, useState, useCallback, useEffect } from 'react';
-import { IconButton, useMediaQuery } from '@material-ui/core';
+import React, { useState, useCallback, useEffect } from 'react';
+import { IconButton } from '@material-ui/core';
 import classNames from 'classnames';
-import { Scrollbars } from 'react-custom-scrollbars';
 
 import { t } from 'modules/i18n/utils/intl';
-import {
-  ITEM_HEIGHT,
-  ANKR_LINK_HEIGHT,
-  useCrossMenuStyles,
-} from './CrossMenuStyles';
-import { useIsMDUp, useIsSMDown } from 'ui';
+import { useIsMDUp } from 'ui';
 import { ReactComponent as MenuIcon } from 'assets/img/menu.svg';
 import { ReactComponent as CloseIcon } from 'assets/img/close.svg';
 import { ReactComponent as AnkrLogo } from 'assets/img/logo/ankr.svg';
-import { ReactComponent as EthereumLogo } from 'assets/img/logo/ethereum.svg';
-import { ReactComponent as SolanaLogo } from 'assets/img/logo/solana.svg';
-import { ReactComponent as BinanceLogo } from 'assets/img/logo/binance.svg';
-import { ReactComponent as PolygonLogo } from 'assets/img/logo/polygon.svg';
-import { ReactComponent as NervosLogo } from 'assets/img/logo/nervos.svg';
-import { ReactComponent as HarmonyLogo } from 'assets/img/logo/harmony.svg';
-import { ReactComponent as FantomLogo } from 'assets/img/logo/fantom.svg';
-import { ReactComponent as AvalancheLogo } from 'assets/img/logo/avalanche.svg';
-import { ReactComponent as IoTexLogo } from 'assets/img/logo/iotex.svg';
-import { ReactComponent as MoonBeamLogo } from 'assets/img/logo/moonbeam.svg';
-import { ReactComponent as ArbitrumLogo } from 'assets/img/logo/arbitrum.svg';
-import { ReactComponent as NearLogo } from 'assets/img/logo/near.svg';
-import { ReactComponent as GnosisLogo } from 'assets/img/logo/gnosis.svg';
-import { ReactComponent as SyscoinLogo } from 'assets/img/logo/syscoin.svg';
-import { ChainId } from 'domains/chains/api/chain';
 import { PROTOCOL_URL } from 'Routes';
+import { useCrossMenuStyles } from './CrossMenuStyles';
+import { MENU_LIST } from './MenuList';
 
 interface ICrossMenuProps {
   chainId: string;
 }
 
 export const CrossMenu = ({ chainId }: ICrossMenuProps) => {
-  const MENU_LIST = useMemo(
-    () => [
-      {
-        chainId: ChainId.Ethereum,
-        name: 'Ethereum',
-        logo: <EthereumLogo />,
-        url: 'http://ethereum.public-rpc.com/',
-      },
-      {
-        chainId: ChainId.Solana,
-        name: 'Solana',
-        logo: <SolanaLogo />,
-        url: 'https://solana.public-rpc.com/ ',
-      },
-      {
-        chainId: ChainId.BSC,
-        name: 'Binance Smart Chain',
-        logo: <BinanceLogo />,
-        url: 'https://bscrpc.com/ ',
-      },
-      {
-        chainId: ChainId.Polygon,
-        name: 'Polygon',
-        logo: <PolygonLogo />,
-        url: 'https://polygon-rpc.com/ ',
-      },
-      {
-        chainId: ChainId.Nervos,
-        name: 'Nervos',
-        logo: <NervosLogo />,
-        url: 'https://nervos.public-rpc.com/',
-      },
-      {
-        chainId: ChainId.Harmony,
-        name: 'Harmony',
-        logo: <HarmonyLogo />,
-        url: 'https://harmony.public-rpc.com/',
-      },
-      {
-        chainId: ChainId.Fantom,
-        name: 'Fantom',
-        logo: <FantomLogo />,
-        url: 'https://rpc.ftm.tools/ ',
-      },
-      {
-        chainId: ChainId.Avalanche,
-        name: 'Avalanche',
-        logo: <AvalancheLogo />,
-        url: 'https://avalanche.public-rpc.com/',
-      },
-      {
-        chainId: ChainId.IoTeX,
-        name: 'IoTeX',
-        logo: <IoTexLogo />,
-        url: 'https://iotexrpc.com/',
-      },
-      {
-        chainId: ChainId.Moonbeam,
-        name: 'MoonBeam',
-        logo: <MoonBeamLogo />,
-        url: 'https://moonbeam.public-rpc.com/',
-      },
-      {
-        chainId: ChainId.Arbitrum,
-        name: 'Arbitrum',
-        logo: <ArbitrumLogo />,
-        url: 'https://arbitrum.public-rpc.com/',
-      },
-      {
-        chainId: ChainId.Near,
-        name: 'Near',
-        logo: <NearLogo />,
-        url: 'https://near.public-rpc.com/',
-      },
-      {
-        chainId: ChainId.Gnosis,
-        name: 'Gnosis',
-        logo: <GnosisLogo />,
-        url: 'https://gnosis.public-rpc.com/',
-      },
-      {
-        chainId: ChainId.Syscoin,
-        name: 'Syscoin',
-        logo: <SyscoinLogo />,
-        url: 'https://syscoin.public-rpc.com/',
-      },
-    ],
-    [],
-  );
-
-  const MENU_HEIGHT = MENU_LIST.length * ITEM_HEIGHT + ANKR_LINK_HEIGHT;
-
-  const classes = useCrossMenuStyles({ menuHeight: MENU_HEIGHT });
-  const isSMDown = useIsSMDown();
-  const isLessThanMaxMenuHeight = useMediaQuery(
-    `@media (max-height: ${MENU_HEIGHT}px)`,
-  );
+  const classes = useCrossMenuStyles();
 
   const [open, setOpen] = useState(false);
 
@@ -160,42 +45,31 @@ export const CrossMenu = ({ chainId }: ICrossMenuProps) => {
         data-test-id="cross-menu-root"
       >
         <div className={classes.menu}>
-          <Scrollbars
-            autoHeightMax={MENU_HEIGHT}
-            autoHeight={!isLessThanMaxMenuHeight && !isSMDown}
-            className={!isLessThanMaxMenuHeight && !isSMDown ? classes.bar : ''}
-            style={
-              isLessThanMaxMenuHeight && !isSMDown
-                ? { height: `calc(100vh - ${ANKR_LINK_HEIGHT}px)` }
-                : {}
-            }
-          >
-            {MENU_LIST.map(item => (
-              <a
-                key={item.chainId}
-                className={classes.item}
-                href={item.url}
-                target="_blank"
-                rel="noopener noreferrer"
-                onClick={handleCloseMenu}
-                data-test-id={
-                  chainId === item.chainId ? 'active-cross-menu' : null
-                }
+          {MENU_LIST.map(item => (
+            <a
+              key={item.chainId}
+              className={classes.item}
+              href={item.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              onClick={handleCloseMenu}
+              data-test-id={
+                chainId === item.chainId ? 'active-cross-menu' : null
+              }
+            >
+              <div
+                className={classNames(
+                  classes.logo,
+                  chainId === item.chainId && classes.current,
+                )}
               >
-                <div
-                  className={classNames(
-                    classes.logo,
-                    chainId === item.chainId && classes.current,
-                  )}
-                >
-                  {item.logo}
-                </div>
-                <div className={classNames(classes.name, chainId)}>
-                  {item.name}
-                </div>
-              </a>
-            ))}
-          </Scrollbars>
+                {item.logo}
+              </div>
+              <div className={classNames(classes.name, chainId)}>
+                {item.name}
+              </div>
+            </a>
+          ))}
         </div>
         <a
           className={classNames(classes.protocol, chainId)}
