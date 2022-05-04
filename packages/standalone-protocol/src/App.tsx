6e20bb9fe4bc143d0d1ff1cb6da93e4633d995b9
evgeniy-bond <evgeniy.bondarev.01@gmail.com>
2022-05-04 13:50:39 +0300

    feat(standalone-protocol): MRPC-866 fix related path for erigonbsc (#823)

--- packages/standalone-protocol/src/App.tsx
+++ packages/standalone-protocol/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { NoSsr } from '@material-ui/core';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 
 import { ScrollToTop } from 'modules/common/components/ScrollToTop';
 import { persistor, store } from './store';
@@ -10,14 +10,14 @@ import { Spinner } from './uiKit/Spinner';
 import { AppBase } from './modules/layout/components/AppBase/AppBase';
 import { Notifications } from './domains/notification/components/Notifications';
 import { Routes } from './Routes';
-import packageJson from '../package.json';
 import { ChainId } from 'domains/chains/api/chain';
+import { historyInstance } from 'modules/common/utils/historyInstance';
 
 const { REACT_APP_CHAIN_ID } = process.env;
 
 function App() {
   return (
-    <Router basename={packageJson.homepage}>
+    <Router history={historyInstance}>
       <Provider store={store}>
         <PersistGate loading={<Spinner />} persistor={persistor}>
           <AppBase chainId={REACT_APP_CHAIN_ID as ChainId}>
