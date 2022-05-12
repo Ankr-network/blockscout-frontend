307b38f70e836a9b9b4e37e061385e3aa9a1676f
Dmitry Kovtunov <kovtunov@ankr.com>
2022-05-12 16:29:26 +0300

    fix(protocol): MRPC-751 Balance rounding

---
+++ packages/protocol/src/domains/account/utils/formatBalance.ts
@@ -0,0 +1,16 @@
+import BigNumber from 'bignumber.js';
+
+import { t } from 'modules/i18n/utils/intl';
+
+// to pervent layout breaking
+const MAX_BALANCE_LENGTH = 6;
+const MAX_DECIMALS_LENGTH = 2;
+
+export const formatBalance = (balance: BigNumber) => {
+  const decimals = Math.min(MAX_DECIMALS_LENGTH, balance.dp());
+  const balanceLength = balance.integerValue().toString().length;
+
+  return balanceLength > MAX_BALANCE_LENGTH
+    ? t('account.balance', { balance: balance.toString() })
+    : balance.toFormat(decimals);
+};
