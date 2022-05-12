2a69b367b727f18d56bda0ab54db4c4f5635a191
Dmitry Kovtunov <kovtunov@ankr.com>
2022-05-12 10:28:16 +0300

    fix(protocol): MPRC-751 'session expired' error fix

--- packages/protocol/src/domains/account/hooks/useBalance.ts
+++ packages/protocol/src/domains/account/hooks/useBalance.ts
@@ -1,3 +1,5 @@
+import { stopPolling } from '@redux-requests/core';
+import { useDispatch } from 'react-redux';
 import { useDispatchRequest, useQuery } from '@redux-requests/react';
 import { useEffect } from 'react';
 
@@ -9,6 +11,8 @@ export interface Balance extends AccountBalance {
   isLoading: boolean;
 }
 
+const actionType = fetchBalance.toString();
+
 export const useBalance = (isConnected: boolean): Balance => {
   const {
     data: balances,
@@ -16,16 +20,21 @@ export const useBalance = (isConnected: boolean): Balance => {
     pristine,
   } = useQuery<AccountBalance>({
     defaultData: defaultBalance,
-    type: fetchBalance.toString(),
+    type: actionType,
   });
 
-  const dispatch = useDispatchRequest();
+  const dispatch = useDispatch();
+  const dispatchRequest = useDispatchRequest();
 
   useEffect(() => {
     if (isConnected) {
-      dispatch(fetchBalance());
+      dispatchRequest(fetchBalance());
     }
-  }, [dispatch, isConnected]);
+
+    return () => {
+      dispatch(stopPolling([actionType]));
+    };
+  }, [dispatch, dispatchRequest, isConnected]);
 
   return { ...balances, isLoading: pristine && loading };
 };
