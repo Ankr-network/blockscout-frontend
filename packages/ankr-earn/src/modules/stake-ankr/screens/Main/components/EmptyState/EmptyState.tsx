3c5c3478a8e1dd086f342227cc1e916b3a1ced37
t.polozov <39823227+PolozovT@users.noreply.github.com>
2022-06-20 19:18:34 +0300

    feat: STAKAN-1661 made how it works dialog ui (#1047)
    * feat: STAKAN-1661 made how it works dialog ui
    
    Co-authored-by: timofei <timofeipolozov@MacBook-Pro.local>
    Co-authored-by: Anton Panov <aliengrey88@gmail.com>

--- packages/ankr-earn/src/modules/stake-ankr/screens/Main/components/EmptyState/EmptyState.tsx
+++ packages/ankr-earn/src/modules/stake-ankr/screens/Main/components/EmptyState/EmptyState.tsx
@@ -3,7 +3,10 @@ import classNames from 'classnames';
 
 import { t } from 'common';
 
-import { NavLink } from 'uiKit/NavLink';
+import { useDialog } from 'modules/common/hooks/useDialog';
+import { Button } from 'uiKit/Button';
+
+import { HowItWorksDialog } from '../HowItWorksDialog';
 
 import { ReactComponent as AnkrBigLogo } from './assets/ankr-logo-big.svg';
 import { Description } from './Description';
@@ -12,27 +15,42 @@ import { useEmptyStateStyles } from './useEmptyStateStyles';
 export const EmptyState = (): JSX.Element => {
   const classes = useEmptyStateStyles();
 
+  const {
+    isOpened: isOpenedHowItWorks,
+    onClose: onCloseHowItWorks,
+    onOpen: onOpenHowItWorks,
+  } = useDialog();
+
   return (
-    <Paper className={classes.paper}>
-      <AnkrBigLogo />
+    <>
+      <Paper className={classes.paper}>
+        <AnkrBigLogo />
 
-      <Typography className={classes.wrapper} variant="h3">
-        {t('stake-ankr.empty-state.start')}
-      </Typography>
+        <Typography className={classes.wrapper} variant="h3">
+          {t('stake-ankr.empty-state.start')}
+        </Typography>
 
-      <Typography className={classes.wrapper}>
-        {t('stake-ankr.empty-state.start-description')}
-      </Typography>
+        <Typography className={classes.wrapper}>
+          {t('stake-ankr.empty-state.start-description')}
+        </Typography>
 
-      <NavLink
-        className={classNames(classes.button, classes.wrapper)}
-        href="stakeLink"
-        variant="outlined"
-      >
-        {t('stake-ankr.empty-state.how-it-works')}
-      </NavLink>
+        <Button
+          className={classNames(classes.button, classes.wrapper)}
+          variant="outlined"
+          onClick={onOpenHowItWorks}
+        >
+          {t('stake-ankr.empty-state.how-it-works')}
+        </Button>
 
-      <Description />
-    </Paper>
+        <Description />
+      </Paper>
+
+      <HowItWorksDialog
+        docsLink="docsLink"
+        nodeProviderLink="nodeProviderLink"
+        open={isOpenedHowItWorks}
+        onClose={onCloseHowItWorks}
+      />
+    </>
   );
 };
