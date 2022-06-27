de6a5dc2ec6870a3ce86218b59aec8806535128f
Denise <41983880+Denise54321@users.noreply.github.com>
2022-06-27 21:34:27 +0800

    feat(protocol): MRPC-805 Combine Standalone Traffic from Legacy PRCs (#1012)

---
+++ packages/protocol/src/domains/chains/actions/fetchTotalRequests.ts
@@ -0,0 +1,51 @@
+import { RequestAction } from '@redux-requests/core';
+import { createAction as createSmartAction } from 'redux-smart-actions';
+
+interface IRequestResult {
+  bytes: number;
+  cachedBytes: number;
+  cachedRequests: number;
+  requests: number;
+}
+interface IEntries {
+  data: IRequestResult;
+  fromTime: string;
+  toTime: string;
+}
+
+interface IFetchTotalRequestsResponseData {
+  entries: IEntries[];
+  totals: IRequestResult;
+}
+
+interface IFetchTotalRequestData {
+  cachedRequests: number;
+  requests: number;
+  totalRequestsHistory: Record<string, number>;
+}
+
+export const fetchTotalRequests = createSmartAction<
+  RequestAction<IFetchTotalRequestsResponseData, IFetchTotalRequestData>
+>('chains/fetchTotalRequests', (url: string) => ({
+  request: {
+    url,
+    method: 'get',
+    data: {},
+  },
+  meta: {
+    driver: 'axios',
+    getData: data => {
+      const totalRequestsHistory: Record<string, number> = {};
+      data.entries.forEach((item: IEntries) => {
+        const key = new Date(item.toTime).getTime().toString();
+        const value = item.data.requests;
+        totalRequestsHistory[key] = value;
+      });
+      return {
+        cachedRequests: data.totals.cachedRequests,
+        requests: data.totals.requests,
+        totalRequestsHistory,
+      };
+    },
+  },
+}));
