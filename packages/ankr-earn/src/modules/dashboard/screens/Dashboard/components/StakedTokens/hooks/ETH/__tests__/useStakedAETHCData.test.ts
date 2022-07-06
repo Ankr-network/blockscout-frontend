d763d4487b8a4b79514c86539a4d0e97f5ee3dfb
tenshou <pamiheevcorp@gmail.com>
2022-07-06 18:11:49 +0700

    feat: STAKAN-1698 fix tests

--- packages/ankr-earn/src/modules/dashboard/screens/Dashboard/components/StakedTokens/hooks/ETH/__tests__/useStakedAETHCData.test.ts
+++ packages/ankr-earn/src/modules/dashboard/screens/Dashboard/components/StakedTokens/hooks/ETH/__tests__/useStakedAETHCData.test.ts
@@ -1,30 +1,52 @@
-import { useDispatchRequest, useQuery } from '@redux-requests/react';
+import {
+  useDispatchRequest,
+  useMutation,
+  useQuery,
+} from '@redux-requests/react';
 import { act, renderHook } from '@testing-library/react-hooks';
 import BigNumber from 'bignumber.js';
 
-import { useStakedBridgeAETHCData } from '../useStakedBridgeAETHCData';
+import { ZERO } from 'modules/common/const';
+
+import { useStakedAETHCData } from '../useStakedAETHCData';
 
 jest.mock('@redux-requests/react', () => ({
   useQuery: jest.fn(),
+  useMutation: jest.fn(),
   useDispatchRequest: jest.fn(),
 }));
 
-jest.mock('modules/bridge/actions/watchAsset', () => ({
-  watchAsset: jest.fn(),
+jest.mock('modules/auth/common/hooks/useConnectedData', () => ({
+  useConnectedData: () => ({ chainId: 1 }),
 }));
 
-jest.mock('modules/dashboard/actions/fetchAETHCBridged', () => ({
-  fetchAETHCBridged: jest.fn(),
+jest.mock('modules/stake-eth/Routes', () => ({
+  RoutesConfig: {
+    stake: { generatePath: () => '/stake' },
+  },
 }));
 
-describe('modules/dashboard/screens/Dashboard/components/StakedTokens/hooks/ETH/useStakedBridgeAETHCData.ts', () => {
+jest.mock('modules/boost/Routes', () => ({
+  RoutesConfig: { tradingCockpit: { generatePath: () => '/trade' } },
+}));
+
+describe('modules/dashboard/screens/Dashboard/components/StakedAETHC/useStakedAETHCData', () => {
   const defaultStatsData = {
     loading: false,
-    data: new BigNumber(1),
+    data: {
+      aETHcBalance: new BigNumber(1),
+      aETHcRatio: new BigNumber(0.5),
+    },
+  };
+
+  const defaultMutationData = {
+    loading: false,
   };
 
   beforeEach(() => {
     (useQuery as jest.Mock).mockReturnValue(defaultStatsData);
+
+    (useMutation as jest.Mock).mockReturnValue(defaultMutationData);
 
     (useDispatchRequest as jest.Mock).mockReturnValue(jest.fn());
   });
@@ -34,26 +56,43 @@ describe('modules/dashboard/screens/Dashboard/components/StakedTokens/hooks/ETH/
   });
 
   test('should return amount and pending value', () => {
-    const { result } = renderHook(() => useStakedBridgeAETHCData());
+    const { result } = renderHook(() => useStakedAETHCData());
+
+    const expectedNativeAmount = new BigNumber(2);
 
     expect(result.current.amount).toStrictEqual(new BigNumber(1));
-    expect(result.current.network).toStrictEqual('Binance SmartChain Testnet');
+    expect(result.current.pendingValue).toStrictEqual(ZERO);
     expect(result.current.isBalancesLoading).toBe(false);
     expect(result.current.isShowed).toBe(true);
-    expect(result.current.chainId).toBe(97);
-    expect(result.current.onAddTokenClick).toBeDefined();
+    expect(result.current.isStakeLoading).toBe(false);
+    expect(result.current.stakeLink).toBe('/stake');
+    expect(result.current.tradeLink).toBe('/trade');
+    expect(result.current.nativeAmount).toStrictEqual(expectedNativeAmount);
+  });
+
+  test('should return zero if there is no data', () => {
+    (useQuery as jest.Mock).mockReturnValue({
+      data: null,
+      loading: false,
+    });
+
+    const { result } = renderHook(() => useStakedAETHCData());
+
+    expect(result.current.amount).toStrictEqual(ZERO);
+    expect(result.current.pendingValue).toStrictEqual(ZERO);
+    expect(result.current.isBalancesLoading).toBe(false);
   });
 
   test('should handle add token to metamask', () => {
     const mockDispatch = jest.fn();
     (useDispatchRequest as jest.Mock).mockReturnValue(mockDispatch);
 
-    const { result } = renderHook(() => useStakedBridgeAETHCData());
+    const { result } = renderHook(() => useStakedAETHCData());
 
     act(() => {
-      result.current.onAddTokenClick();
+      result.current.handleAddTokenToWallet();
     });
 
-    expect(mockDispatch).toBeCalledTimes(2);
+    expect(mockDispatch).toBeCalledTimes(1);
   });
 });
--- packages/ankr-earn/src/modules/dashboard/screens/Dashboard/components/StakedTokens/hooks/ETH/__tests__/useStakedBridgeAETHCData.test.ts
+++ packages/ankr-earn/src/modules/dashboard/screens/Dashboard/components/StakedTokens/hooks/ETH/__tests__/useStakedBridgeAETHCData.test.ts
@@ -1,52 +1,30 @@
-import {
-  useDispatchRequest,
-  useMutation,
-  useQuery,
-} from '@redux-requests/react';
+import { useDispatchRequest, useQuery } from '@redux-requests/react';
 import { act, renderHook } from '@testing-library/react-hooks';
 import BigNumber from 'bignumber.js';
 
-import { ZERO } from 'modules/common/const';
-
-import { useStakedAETHCData } from '../useStakedAETHCData';
+import { useStakedBridgeAETHCData } from '../useStakedBridgeAETHCData';
 
 jest.mock('@redux-requests/react', () => ({
   useQuery: jest.fn(),
-  useMutation: jest.fn(),
   useDispatchRequest: jest.fn(),
 }));
 
-jest.mock('modules/auth/common/hooks/useConnectedData', () => ({
-  useConnectedData: () => ({ chainId: 1 }),
+jest.mock('modules/bridge/actions/watchAsset', () => ({
+  watchAsset: jest.fn(),
 }));
 
-jest.mock('modules/stake-eth/Routes', () => ({
-  RoutesConfig: {
-    stake: { generatePath: () => '/stake' },
-  },
+jest.mock('modules/dashboard/actions/fetchAETHCBridged', () => ({
+  fetchAETHCBridged: jest.fn(),
 }));
 
-jest.mock('modules/boost/Routes', () => ({
-  RoutesConfig: { tradingCockpit: { generatePath: () => '/trade' } },
-}));
-
-describe('modules/dashboard/screens/Dashboard/components/StakedAETHC/useStakedAETHCData', () => {
+describe('modules/dashboard/screens/Dashboard/components/StakedTokens/hooks/ETH/useStakedBridgeAETHCData.ts', () => {
   const defaultStatsData = {
     loading: false,
-    data: {
-      aETHcBalance: new BigNumber(1),
-      aETHcRatio: new BigNumber(0.5),
-    },
-  };
-
-  const defaultMutationData = {
-    loading: false,
+    data: new BigNumber(1),
   };
 
   beforeEach(() => {
     (useQuery as jest.Mock).mockReturnValue(defaultStatsData);
-
-    (useMutation as jest.Mock).mockReturnValue(defaultMutationData);
 
     (useDispatchRequest as jest.Mock).mockReturnValue(jest.fn());
   });
@@ -56,43 +34,26 @@ describe('modules/dashboard/screens/Dashboard/components/StakedAETHC/useStakedAE
   });
 
   test('should return amount and pending value', () => {
-    const { result } = renderHook(() => useStakedAETHCData());
-
-    const expectedNativeAmount = new BigNumber(2);
+    const { result } = renderHook(() => useStakedBridgeAETHCData());
 
     expect(result.current.amount).toStrictEqual(new BigNumber(1));
-    expect(result.current.pendingValue).toStrictEqual(ZERO);
+    expect(result.current.network).toStrictEqual('Binance SmartChain Testnet');
     expect(result.current.isBalancesLoading).toBe(false);
     expect(result.current.isShowed).toBe(true);
-    expect(result.current.isStakeLoading).toBe(false);
-    expect(result.current.stakeLink).toBe('/stake');
-    expect(result.current.tradeLink).toBe('/trade');
-    expect(result.current.nativeAmount).toStrictEqual(expectedNativeAmount);
-  });
-
-  test('should return zero if there is no data', () => {
-    (useQuery as jest.Mock).mockReturnValue({
-      data: null,
-      loading: false,
-    });
-
-    const { result } = renderHook(() => useStakedAETHCData());
-
-    expect(result.current.amount).toStrictEqual(ZERO);
-    expect(result.current.pendingValue).toStrictEqual(ZERO);
-    expect(result.current.isBalancesLoading).toBe(false);
+    expect(result.current.chainId).toBe(97);
+    expect(result.current.onAddTokenClick).toBeDefined();
   });
 
   test('should handle add token to metamask', () => {
     const mockDispatch = jest.fn();
     (useDispatchRequest as jest.Mock).mockReturnValue(mockDispatch);
 
-    const { result } = renderHook(() => useStakedAETHCData());
+    const { result } = renderHook(() => useStakedBridgeAETHCData());
 
     act(() => {
-      result.current.handleAddTokenToWallet();
+      result.current.onAddTokenClick();
     });
 
-    expect(mockDispatch).toBeCalledTimes(1);
+    expect(mockDispatch).toBeCalledTimes(2);
   });
 });
