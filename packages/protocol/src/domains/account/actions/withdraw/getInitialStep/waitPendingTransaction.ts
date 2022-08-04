219ec3557786b7b58a7b95b1ad988650e6a45979
evgeniy-bond <evgeniy.bondarev.01@gmail.com>
2022-08-04 16:45:39 +0300

    fix(MRPC-1343): Add loop for  waitPendingTransaction and timeouts (#1371)

--- packages/protocol/src/domains/account/actions/withdraw/getInitialStep/waitPendingTransaction.ts
+++ packages/protocol/src/domains/account/actions/withdraw/getInitialStep/waitPendingTransaction.ts
@@ -1,12 +1,13 @@
 import Web3 from 'web3';
 
 import { MultiService } from 'modules/api/MultiService';
-import { retry } from 'modules/api/utils/retry';
 import { API_ENV } from 'modules/common/utils/environment';
+
+const ETH_BLOCK_TIME = 10_000;
 
 export const web3 = new Web3(
   API_ENV === 'prod'
-    ? 'https://rpc.ankr.com/eth/22e282df02e47a6dc906c48db9830304e93e9f12bb74a179152c747c01d4e7b7'
+    ? 'https://rpc.ankr.com/eth'
     : 'https://rpc.ankr.com/eth_goerli',
 );
 
@@ -30,26 +31,22 @@ const hasPendingTransaction = async () => {
 
 const TIMEOUT_FOR_TRANSACTION = 3000;
 
-const timeout = () =>
-  new Promise(res => setTimeout(res, TIMEOUT_FOR_TRANSACTION));
+const timeout = (ms = TIMEOUT_FOR_TRANSACTION) =>
+  new Promise(res => setTimeout(res, ms));
 
 export const waitPendingTransaction = async () => {
   await timeout();
 
-  const inProcess = await hasPendingTransaction();
+  let inProcess = true;
 
-  if (!inProcess) return undefined;
+  while (inProcess) {
+    // eslint-disable-next-line
+    inProcess = await hasPendingTransaction();
 
-  return retry(
-    async () => {
-      const stillInPending = await hasPendingTransaction();
+    // eslint-disable-next-line
+    await timeout(ETH_BLOCK_TIME);
+  }
 
-      if (stillInPending) {
-        throw new Error();
-      }
-
-      return inProcess;
-    },
-    () => false,
-  );
+  // because we need to wait other nodes
+  await timeout();
 };
