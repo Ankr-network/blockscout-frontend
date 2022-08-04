fd3db4c392194a5265e7f59aced53cc3b3a3e7b4
t.polozov <39823227+PolozovT@users.noreply.github.com>
2022-08-04 10:20:13 +0300

    fix: STAKAN-1648 fixed url for price data (#1367)
    * fix: STAKAN- 1648 fixed url for anrk price request

--- packages/ankr-earn/src/modules/stake-ankr/actions/getANKRPrice.ts
+++ packages/ankr-earn/src/modules/stake-ankr/actions/getANKRPrice.ts
@@ -2,8 +2,6 @@ import { RequestAction } from '@redux-requests/core';
 import BigNumber from 'bignumber.js';
 import { createAction as createSmartAction } from 'redux-smart-actions';
 
-import { isMainnet, ZERO } from 'modules/common/const';
-
 import { ANKR_ACTIONS_PREFIX, ANKR_TOKEN_PRICE } from '../const';
 
 interface IGetANKRPrice {
@@ -15,11 +13,7 @@ export const getANKRPrice = createSmartAction<
 >(
   `${ANKR_ACTIONS_PREFIX}getANKRPrice`,
   (): RequestAction => ({
-    request: isMainnet
-      ? { url: ANKR_TOKEN_PRICE }
-      : {
-          promise: Promise.resolve(ZERO),
-        },
+    request: { url: ANKR_TOKEN_PRICE },
     meta: {
       driver: 'axios',
       showNotificationOnError: false,
