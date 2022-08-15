f3cd91714c4d33ab3e049c1f90944cd97a2ba1be
evgeniy-bond <evgeniy.bondarev.01@gmail.com>
2022-08-15 14:27:50 +0300

    feat(protocol): MRPC-1311 low balance settings updates (#1461)

--- packages/protocol/src/domains/userSettings/screens/components/NotificationsBlock/NotificationsForm/NotificationsFormTypes.ts
+++ packages/protocol/src/domains/userSettings/screens/components/NotificationsBlock/NotificationsForm/NotificationsFormTypes.ts
@@ -3,11 +3,17 @@ import { INotificationsSettings } from 'multirpc-sdk';
 export enum NotificationsFormFields {
   balance = 'balance',
   marketing = 'marketing',
+  lowBalance = 'lowBalance',
+  lowBalance3days = 'lowBalance3days',
+  lowBalance7days = 'lowBalance7days',
 }
 
 export interface NotificationsFormData {
   [NotificationsFormFields.balance]: boolean;
   [NotificationsFormFields.marketing]: boolean;
+  [NotificationsFormFields.lowBalance]: boolean;
+  [NotificationsFormFields.lowBalance3days]: boolean;
+  [NotificationsFormFields.lowBalance7days]: boolean;
 }
 
 export interface INotificationsFormProps {
