8617255d68c31a7188e2a37d36a1ac3c15ada9fd
t.polozov <39823227+PolozovT@users.noreply.github.com>
2022-08-17 11:31:18 +0300

    feat: STAKAN-1920 added token info dialog for ankr (#1521)

--- packages/ankr-earn/src/modules/dashboard/screens/Dashboard/components/StakedANKR/StakedANKR.tsx
+++ packages/ankr-earn/src/modules/dashboard/screens/Dashboard/components/StakedANKR/StakedANKR.tsx
@@ -1,6 +1,14 @@
-import { t } from 'common';
+import { Box } from '@material-ui/core';
 
-import { DEFAULT_ROUNDING, ETH_NETWORK_BY_ENV } from 'modules/common/const';
+import { t, tHTML } from 'common';
+
+import { configFromEnv } from 'modules/api/config';
+import {
+  DEFAULT_ROUNDING,
+  DOCS_ANKR_TOKEN_STAKING_LINK,
+  ETH_NETWORK_BY_ENV,
+} from 'modules/common/const';
+import { useDialog } from 'modules/common/hooks/useDialog';
 import { Token } from 'modules/common/types/token';
 import { Amount } from 'modules/dashboard/components/Amount';
 import {
@@ -8,6 +16,8 @@ import {
   DashboardCardSkeleton,
 } from 'modules/dashboard/components/DashboardCard';
 import { NetworkIconText } from 'modules/dashboard/components/NetworkIconText';
+import { TokenInfoDialog } from 'modules/dashboard/components/TokenInfoDialog';
+import { Menu } from 'uiKit/Menu';
 import { NavLink } from 'uiKit/NavLink';
 
 import { useStakedANKRData } from '../StakedTokens/hooks/ANKR/useStakedANKRData';
@@ -15,7 +25,14 @@ import { useStakedANKRData } from '../StakedTokens/hooks/ANKR/useStakedANKRData'
 import { useStakedANKRStyles } from './useStakedANKRStyles';
 
 export const StakedANKR = (): JSX.Element => {
+  const { contractConfig } = configFromEnv();
   const classes = useStakedANKRStyles();
+
+  const {
+    isOpened: isOpenedInfo,
+    onClose: onCloseInfo,
+    onOpen: onOpenInfo,
+  } = useDialog();
 
   const { stakedAmount, stakedUsdEquivalent, network, manageLink, loading } =
     useStakedANKRData();
@@ -29,26 +46,48 @@ export const StakedANKR = (): JSX.Element => {
   });
 
   return (
-    <DashboardCard
-      amountSlot={
-        <Amount infoSlot={usdValue} value={stakedAmount.integerValue()} />
-      }
-      buttonsSlot={
-        <NavLink
-          className={classes.manageButton}
-          href={manageLink}
-          variant="outlined"
-        >
-          {t('dashboard.card.manage')}
-        </NavLink>
-      }
-      networkAndIconSlot={
-        <NetworkIconText
-          chainId={ETH_NETWORK_BY_ENV}
-          network={network}
-          token={Token.ANKR}
-        />
-      }
-    />
+    <>
+      <DashboardCard
+        amountSlot={
+          <Amount infoSlot={usdValue} value={stakedAmount.integerValue()} />
+        }
+        buttonsSlot={
+          <NavLink
+            className={classes.manageButton}
+            href={manageLink}
+            variant="outlined"
+          >
+            {t('dashboard.card.manage')}
+          </NavLink>
+        }
+        menuSlot={
+          <Box component="span" display="flex">
+            <Menu>
+              {[
+                <Menu.Item disabled={!onOpenInfo} onClick={onOpenInfo}>
+                  {t('dashboard.card.tokenInfo')}
+                </Menu.Item>,
+              ]}
+            </Menu>
+          </Box>
+        }
+        networkAndIconSlot={
+          <NetworkIconText
+            chainId={ETH_NETWORK_BY_ENV}
+            network={network}
+            token={Token.ANKR}
+          />
+        }
+      />
+
+      <TokenInfoDialog
+        description={tHTML('dashboard.token-info.ANKR')}
+        moreHref={DOCS_ANKR_TOKEN_STAKING_LINK}
+        open={isOpenedInfo}
+        tokenAddress={contractConfig.ankrToken}
+        tokenName={Token.ANKR}
+        onClose={onCloseInfo}
+      />
+    </>
   );
 };
