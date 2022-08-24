eed3b01f5f39d0e2a0c728a148bf0993cd8b031c
t.polozov <39823227+PolozovT@users.noreply.github.com>
2022-08-24 09:58:46 +0300

    feat: STAKAN-1986 changed sdk to read again (#1602)
    * feat: STAKAN-1986 changed sdk to read again

--- packages/ankr-earn/src/modules/stake-ankr/actions/getTotalTvl.ts
+++ packages/ankr-earn/src/modules/stake-ankr/actions/getTotalTvl.ts
@@ -2,7 +2,7 @@ import { RequestAction } from '@redux-requests/core';
 import BigNumber from 'bignumber.js';
 import { createAction } from 'redux-smart-actions';
 
-import { AnkrStakingSDK } from '../api/AnkrStakingSDK';
+import { AnkrStakingReadSDK } from '../api/AnkrStakingSDK';
 import { ANKR_ACTIONS_PREFIX } from '../const';
 
 export const getTotalTvl = createAction<RequestAction<BigNumber, BigNumber>>(
@@ -10,7 +10,7 @@ export const getTotalTvl = createAction<RequestAction<BigNumber, BigNumber>>(
   () => ({
     request: {
       promise: (async (): Promise<BigNumber> => {
-        const sdk = await AnkrStakingSDK.getInstance();
+        const sdk = await AnkrStakingReadSDK.getInstance();
         const provider = await sdk.getProvider();
 
         const latestBlockNumber = await provider.getBlockNumber();
