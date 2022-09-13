8938eb8a06f53ac22a0cbc8ef6530de4440922d9
Gregory <GregoryBai@outlook.com>
2022-09-13 10:34:55 +0300

    fix(protocol): MRPC-1537 React snap page generation. TypeError: n.replaceAll (#1796)
    * fix: MRPC-1537 debug via console.logs
    
    * fix: MRPC-1537 prevent errors from warning for test build
    
    * fix: MRPC-1537 move console.log
    
    * fix: MRPC-1537 add console.log for QUERY_EMAIL
    
    * fix: MRPC-1537 replace .replaceAll() with .split().join()

--- packages/protocol/src/domains/userSettings/components/GuardAuthUserSettingsRoute/utils/isInvitation.ts
+++ packages/protocol/src/domains/userSettings/components/GuardAuthUserSettingsRoute/utils/isInvitation.ts
@@ -1,13 +1,14 @@
+/* eslint-disable no-console */
 import { RouteProps } from 'react-router';
 
-import { PATH_SETTINGS } from 'domains/userSettings/Routes';
 import { QUERY_EMAIL } from 'domains/userSettings/const';
+import { PATH_SETTINGS } from 'domains/userSettings/Routes';
 
 export const isInvitation = ({ location }: RouteProps) => {
   const { pathname = '', search = '' } = location || {};
 
   const isSettingsPath =
-    pathname.replaceAll('/', '') === PATH_SETTINGS.replaceAll('/', '');
+    pathname.split('/').join('') === PATH_SETTINGS.split('/').join('');
 
   const hasEmailParam = !!new URLSearchParams(search).get(QUERY_EMAIL);
 
