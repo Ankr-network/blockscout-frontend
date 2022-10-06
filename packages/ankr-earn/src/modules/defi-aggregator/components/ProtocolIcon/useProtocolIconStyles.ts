bf2a1eb2ab44ce90585adc4ee3d0d38f249cf518
Anton Panov <aliengrey88@gmail.com>
2022-10-06 11:36:28 +0300

    feat(staking/defi): STAKAN-2138 add new platform to defi aggregator (#1973)
    * feat(defi): add new platforms
    
    * chore: prettify
    
    * feat: add midas protocol
    
    * feat: add landing type

---
+++ packages/ankr-earn/src/modules/defi-aggregator/components/ProtocolIcon/useProtocolIconStyles.ts
@@ -0,0 +1,9 @@
+import { makeStyles } from '@material-ui/core';
+
+export const useProtocolIconStyles = makeStyles(() => ({
+  root: {
+    width: 24,
+    height: 24,
+    objectFit: 'contain',
+  },
+}));
