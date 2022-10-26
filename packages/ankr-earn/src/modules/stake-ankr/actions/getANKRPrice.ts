0072e10adf9cee76a0dfc5621c981270b4560b76
Anton Panov <aliengrey88@gmail.com>
2022-10-26 11:23:26 +0300

    fix(staking): STAKAN-2179 use propper gateway url (#2098)

--- packages/ankr-earn/src/modules/stake-ankr/actions/getANKRPrice.ts
+++ packages/ankr-earn/src/modules/stake-ankr/actions/getANKRPrice.ts
@@ -1,6 +1,12 @@
 import BigNumber from 'bignumber.js';
 
+import { configFromEnv } from 'modules/api/config';
+
 import { web3Api } from '../../api/web3Api';
+
+const { baseUrl } = configFromEnv().gatewayConfig;
+
+const ANKR_RATE_URL = '/v1alpha/rate/ANKR';
 
 interface IGetANKRPrice {
   rate: number;
@@ -9,7 +15,7 @@ interface IGetANKRPrice {
 export const { useGetAnkrPriceQuery } = web3Api.injectEndpoints({
   endpoints: build => ({
     getAnkrPrice: build.query<BigNumber, void>({
-      query: () => 'https://api.stkr.io/v1alpha/rate/ANKR',
+      query: () => new URL(ANKR_RATE_URL, baseUrl).toString(),
       transformResponse: (data: IGetANKRPrice) => new BigNumber(data.rate),
     }),
   }),
