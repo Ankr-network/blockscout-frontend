0c32c58bca3951c1d501395c944c2d8960a50623
t.polozov <39823227+PolozovT@users.noreply.github.com>
2022-11-17 17:09:35 +0300

    feat: STAKAN-2304 added success step page and icons (#2235)

--- packages/ankr-earn/src/modules/stake-sui/screens/StakeSuiSteps/StakeSuiSteps.tsx
+++ packages/ankr-earn/src/modules/stake-sui/screens/StakeSuiSteps/StakeSuiSteps.tsx
@@ -1,3 +1,37 @@
+import { t } from 'common';
+
+import { ProgressStep } from 'modules/common/components/ProgressStep';
+import { Token } from 'modules/common/types/token';
+
+import { useStakeSuiStepsHook } from './useStakeSuiStepsHook';
+
+const SYNTH_TOKEN = Token.aSUIc;
+
 export const StakeSuiSteps = (): JSX.Element => {
-  return <div>StakeSuiSteps</div>;
+  const {
+    isLoading,
+    isPending,
+    amount,
+    destination,
+    error,
+    transactionId,
+    handleAddTokenToWallet,
+  } = useStakeSuiStepsHook();
+
+  return (
+    <ProgressStep
+      amount={amount}
+      buttonTitle={t('stake.buttons.addToWallet', { token: SYNTH_TOKEN })}
+      destinationAddress={destination}
+      // TODO: need to double check and test it
+      error={error as unknown as Error}
+      hint={t('stake.pending.description', { token: SYNTH_TOKEN })}
+      isLoading={isLoading}
+      isPending={isPending}
+      symbol={SYNTH_TOKEN}
+      title={t('stake.progressTitle')}
+      txHash={transactionId}
+      onAddTokenToWallet={handleAddTokenToWallet}
+    />
+  );
 };
