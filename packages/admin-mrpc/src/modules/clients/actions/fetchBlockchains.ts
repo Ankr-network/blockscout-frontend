ec82a27f4a5f2dd02a2aceb5a7ea37918c721940
Dmitriy Lisyanskiy <dmitriy.lisyanskiy@ankr.com>
2022-11-28 19:38:55 +0400

    chore(admin-mrpc): fix blockchains api endpoints generating

--- packages/admin-mrpc/src/modules/clients/actions/fetchBlockchains.ts
+++ packages/admin-mrpc/src/modules/clients/actions/fetchBlockchains.ts
@@ -1,11 +1,9 @@
-import { Web3Address } from 'multirpc-sdk';
 import { web3Api } from 'store/queries/web3Api';
 import { MultiService } from 'modules/api/MultiService';
 import { filterMapChains, IApiChain } from '../utils/queryChains';
 
 interface IRequestParams {
   token: string;
-  address: Web3Address;
 }
 
 export const {
@@ -14,17 +12,12 @@ export const {
 } = web3Api.injectEndpoints({
   endpoints: build => ({
     fetchBlockchains: build.query<IApiChain[], IRequestParams>({
-      queryFn: async ({ token, address }) => {
+      queryFn: async ({ token }) => {
         const service = await MultiService.getService();
         const blockchains = await service.getPublicGateway().getBlockchains();
         const formattedPrivateChains = await service.formatPrivateEndpoints(
           blockchains,
-          // we need only user token string here to generate the chain urls
-          // @ts-ignore
-          {
-            owner_address: address,
-            endpoint_token: token,
-          },
+          token,
         );
         const filteredChains = filterMapChains(formattedPrivateChains);
 
