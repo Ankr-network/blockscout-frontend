9436cda31d7f06584a81b066724a012d5b4f5367
Dmitriy Lisyanskiy <dmitriy.lisyanskiy@ankr.com>
2022-12-01 16:46:40 +0400

    feat(admin-mrpc): MRPC-1945 sync values and labels with main MRPC interface

--- packages/admin-mrpc/src/modules/clients/components/ClientDetailsPage/ClientBalancesModal/ClientBalancesInfo.tsx
+++ packages/admin-mrpc/src/modules/clients/components/ClientDetailsPage/ClientBalancesModal/ClientBalancesInfo.tsx
@@ -2,6 +2,7 @@ import { Typography, Grid, GridSize, Box } from '@mui/material';
 import { renderBalance, renderUSD } from 'modules/common/utils/renderBalance';
 import { ClientMapped } from 'modules/clients/store/clientsSlice';
 import { useClientDetailsStyles as useStyles } from '../ClientDetailsStyles';
+import { columns } from '../../ClientsTable/clientTableUtils';
 
 interface IClientBalancesInfoProps {
   currentClient: ClientMapped;
@@ -18,7 +19,8 @@ export const ClientBalancesInfo = ({
       <Grid className={classes.balancesGridWrapper} container spacing={3}>
         <Grid item xs={size}>
           <Typography variant="subtitle2" component="p">
-            Total amount of credits
+            {columns.find(column => column.key === 'amount')?.label ||
+              'Total amount of Credits'}
           </Typography>
           <Typography variant="body2">
             {renderBalance(currentClient.amount)}
@@ -26,7 +28,8 @@ export const ClientBalancesInfo = ({
         </Grid>
         <Grid item xs={size}>
           <Typography variant="subtitle2" component="p">
-            Amount of voucher credits
+            {columns.find(column => column.key === 'voucherAmount')?.label ||
+              'Voucher credits'}
           </Typography>
           <Typography variant="body2">
             {renderBalance(currentClient.voucherAmount)}
@@ -34,10 +37,20 @@ export const ClientBalancesInfo = ({
         </Grid>
         <Grid item xs={size}>
           <Typography variant="subtitle2" component="p">
-            Equivalent in USD
+            {columns.find(column => column.key === 'amountUsd')?.label ||
+              'Balance in USD'}
           </Typography>
           <Typography variant="body2">
             {renderUSD(currentClient.amountUsd)}
+          </Typography>
+        </Grid>
+        <Grid item xs={size}>
+          <Typography variant="subtitle2" component="p">
+            {columns.find(column => column.key === 'amountAnkr')?.label ||
+              'Balance in ANKR'}
+          </Typography>
+          <Typography variant="body2">
+            {renderBalance(currentClient.amountAnkr)}
           </Typography>
         </Grid>
       </Grid>
