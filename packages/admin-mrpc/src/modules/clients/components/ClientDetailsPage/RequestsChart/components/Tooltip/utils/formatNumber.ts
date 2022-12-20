3c391c4bad36c24210d2bb05b193e4ccfa94d27c
Dmitriy Lisyanskiy <dmitriy.lisyanskiy@ankr.com>
2022-12-20 18:39:21 +0400

    feat(admin-mrpc): MRPC-1981 wrap formatNumber into try/catch

--- packages/admin-mrpc/src/modules/clients/components/ClientDetailsPage/RequestsChart/components/Tooltip/utils/formatNumber.ts
+++ packages/admin-mrpc/src/modules/clients/components/ClientDetailsPage/RequestsChart/components/Tooltip/utils/formatNumber.ts
@@ -1,4 +1,9 @@
 import BigNumber from 'bignumber.js';
 
-export const formatNumber = (number: string | number) =>
-  new BigNumber(number).toFormat(0);
+export const formatNumber = (number: string | number) => {
+  try {
+    return new BigNumber(number).toFormat(0);
+  } catch (e) {
+    return number;
+  }
+};
