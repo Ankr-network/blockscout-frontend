133dd91e84ce0433cb291b4b199931aa81e9ffa5
Dmitriy Lisyanskiy <dmitriy.lisyanskiy@ankr.com>
2023-02-16 19:41:34 +0700

    feat(admin-mrpc): MRPC-2320 chore

--- packages/admin-mrpc/src/modules/clients/components/ClientDetailsPage/ClientBalancesModal/useClientBalancesModalContent.ts
+++ packages/admin-mrpc/src/modules/clients/components/ClientDetailsPage/ClientBalancesModal/useClientBalancesModalContent.ts
@@ -1,4 +1,4 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent, useCallback, useState } from 'react';
 import { toast } from 'react-toastify';
 import { millisecondsToSeconds } from 'date-fns';
 
@@ -22,7 +22,7 @@ interface IFormElements {
 }
 
 export const useClientBalancesModalContent = (
-  currentClient: ClientMapped,
+  { address }: ClientMapped,
   onClose: () => void,
 ) => {
   const [addUserVoucherCredits, { isLoading: isLoadingAddCredits }] =
@@ -31,80 +31,91 @@ export const useClientBalancesModalContent = (
     useSubtractUserVoucherCreditsMutation();
   const { refetch: refetchClients } = useFetchCountersQuery();
   const { refetch: refetchTransactions } = useFetchUserTransactionsQuery({
-    address: currentClient.address!,
+    address: address!,
   });
 
   const isLoading = isLoadingAddCredits || isLoadingSubtractCredits;
 
   const [amount, setAmount] = useState<number | undefined>(undefined);
 
-  const handleSubmit = (
-    e: FormEvent<HTMLFormElement> & {
-      target: IFormElements;
-      nativeEvent: SubmitEvent;
-    },
-  ) => {
-    e.preventDefault();
+  const handleSubmit = useCallback(
+    (
+      e: FormEvent<HTMLFormElement> & {
+        target: IFormElements;
+        nativeEvent: SubmitEvent;
+      },
+    ) => {
+      e.preventDefault();
 
-    // getting id of submit button for add or subtract credits
-    const submitterId = e.nativeEvent.submitter?.id as
-      | typeof ADD_CREDITS_ID
-      | typeof SUBTRACT_CREDITS_ID
-      | undefined;
+      // getting id of submit button for add or subtract credits
+      const submitterId = e.nativeEvent.submitter?.id as
+        | typeof ADD_CREDITS_ID
+        | typeof SUBTRACT_CREDITS_ID
+        | undefined;
 
-    const {
-      comment: { value: commentValue },
-      validDuringDays: { value: validDuringDaysValue },
-    } = e.target.elements;
+      const {
+        comment: { value: commentValue },
+        validDuringDays: { value: validDuringDaysValue },
+      } = e.target.elements;
 
-    if (!currentClient.address) {
-      toast.error("Can't find user address");
-      return;
-    }
-
-    if (!amount) {
-      toast.error('amount field is required');
-      return;
-    }
-
-    let expiresAt = '0';
-    if (validDuringDaysValue) {
-      if (+validDuringDaysValue > 30) {
-        toast.error('maximum is 30 days');
+      if (!address) {
+        toast.error("Can't find user address");
         return;
       }
-      const newDate = new Date();
-      newDate.setDate(newDate.getDate() + +validDuringDaysValue);
-      const expiresAtSeconds = Math.floor(
-        millisecondsToSeconds(newDate.getTime()),
-      );
-      expiresAt = expiresAtSeconds.toString();
-    }
 
-    const requestParams = {
-      address: currentClient.address,
-      amountType: 'credit' as IAmountType,
-      amount: amount.toString(),
-      reasonId: `${Date.now()} ${commentValue || ''}`,
-      expiresAt,
-    };
-
-    const handleResponse = (res: any) => {
-      if ('data' in res && res.data.success) {
-        refetchClients();
-        refetchTransactions();
-        onClose();
+      if (!amount) {
+        toast.error('amount field is required');
+        return;
       }
-    };
 
-    if (submitterId === ADD_CREDITS_ID) {
-      addUserVoucherCredits(requestParams).then(handleResponse);
-    }
+      let expiresAt = '0';
+      if (validDuringDaysValue) {
+        if (+validDuringDaysValue > 30) {
+          toast.error('maximum is 30 days');
+          return;
+        }
+        const newDate = new Date();
+        newDate.setDate(newDate.getDate() + +validDuringDaysValue);
+        const expiresAtSeconds = Math.floor(
+          millisecondsToSeconds(newDate.getTime()),
+        );
+        expiresAt = expiresAtSeconds.toString();
+      }
 
-    if (submitterId === SUBTRACT_CREDITS_ID) {
-      subtractUserVoucherCredits(requestParams).then(handleResponse);
-    }
-  };
+      const requestParams = {
+        address,
+        amountType: 'credit' as IAmountType,
+        amount: amount.toString(),
+        reasonId: `${Date.now()} ${commentValue || ''}`,
+        expiresAt,
+      };
+
+      const handleResponse = (res: any) => {
+        if ('data' in res && res.data.success) {
+          refetchClients();
+          refetchTransactions();
+          onClose();
+        }
+      };
+
+      if (submitterId === ADD_CREDITS_ID) {
+        addUserVoucherCredits(requestParams).then(handleResponse);
+      }
+
+      if (submitterId === SUBTRACT_CREDITS_ID) {
+        subtractUserVoucherCredits(requestParams).then(handleResponse);
+      }
+    },
+    [
+      addUserVoucherCredits,
+      amount,
+      address,
+      onClose,
+      refetchClients,
+      refetchTransactions,
+      subtractUserVoucherCredits,
+    ],
+  );
 
   return {
     handleSubmit,
