8ce26ff24526375953034dab55c49aa1412ff052
Denise <41983880+Denise54321@users.noreply.github.com>
2023-02-17 20:39:44 +0800

    feat(protocol): MRPC-2350 Add Upgrade info into Ws filed (#2811)

--- packages/protocol/src/domains/chains/screens/ChainItem/components/GetStartedSection/components/WSEndpoints/useWsEndpointsStyles.ts
+++ packages/protocol/src/domains/chains/screens/ChainItem/components/GetStartedSection/components/WSEndpoints/useWsEndpointsStyles.ts
@@ -1,8 +1,7 @@
-import { Theme } from '@mui/material/styles';
 import { makeStyles } from 'tss-react/mui';
 
-export const useWSEndpointsStyles = makeStyles()((theme: Theme) => ({
-  wsEndpoints: {
+export const useWsEdnpointsStyles = makeStyles()(theme => ({
+  root: {
     display: 'flex',
     flexDirection: 'column',
     gap: theme.spacing(2 * 1.75),
