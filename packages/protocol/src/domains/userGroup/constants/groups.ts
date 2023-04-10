ec23d58112ee95446265ac33a0a16c4f89a4b2f9
Dmitriy Lisyanskiy <baomastr@users.noreply.github.com>
2023-04-10 21:27:15 +0400

    fix(protocol): MRPC-2711 upgrade plan for free users (#3249) (#3250)

--- packages/protocol/src/domains/userGroup/constants/groups.ts
+++ packages/protocol/src/domains/userGroup/constants/groups.ts
@@ -3,7 +3,6 @@ export enum BlockWithPermission {
   UsageData, // access to the Telemetry,
   Billing, // access to the Billing information (account details page)
   Payment, // access to the payment form
-  Disabled, // access disabled if group is selected (until https://ankrnetwork.atlassian.net/browse/MRPC-2703 will be resolved)
 }
 
 export const OWNER_PERMISSIONS = [
