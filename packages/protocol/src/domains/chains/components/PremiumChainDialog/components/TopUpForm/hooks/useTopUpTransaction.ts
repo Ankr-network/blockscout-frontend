f9a25fe9716fbe07b43b625a88af5fc3ea57bda1
evgeniy-bond <evgeniy.bondarev.01@gmail.com>
2023-04-18 10:19:45 +0200

    fix(mrpc): MRPC-2743 Remove logic related to checking token issuence … (#3301)
    * feat(mrpc): MRPC-2702 add instant jwt participant login
    
    * fix(mrpc): MRPC-2702 chore
    
    * fix(mrpc): MRPC-2743 Remove logic related to checking token issuence of topup

--- packages/protocol/src/domains/chains/components/PremiumChainDialog/components/TopUpForm/hooks/useTopUpTransaction.ts
+++ packages/protocol/src/domains/chains/components/PremiumChainDialog/components/TopUpForm/hooks/useTopUpTransaction.ts
@@ -1,15 +1,13 @@
-import { useCheckLoginStep } from './useCheckLoginStep';
 import { useSelectTopUpTransaction } from 'domains/account/hooks/useSelectTopUpTransaction';
 
 export const useTopUpTransaction = () => {
   const transaction = useSelectTopUpTransaction();
-  const { hasLoginStep } = useCheckLoginStep();
 
   const { amount, allowanceTransactionHash, topUpTransactionHash } =
     transaction || {};
 
   const isTopUpInProcess = Boolean(
-    allowanceTransactionHash || topUpTransactionHash || hasLoginStep,
+    allowanceTransactionHash || topUpTransactionHash,
   );
 
   return { amount, isTopUpInProcess };
