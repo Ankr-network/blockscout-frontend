a69ddf390628b5c249bbb81b5471a61bcd851a58
Denise <41983880+Denise54321@users.noreply.github.com>
2023-04-24 16:37:09 +0800

    fix(protocol): MRPC-2723 Use Public URL to fetch Last Block Number (#3306)

--- packages/protocol/src/domains/chains/utils/chainsUtils.ts
+++ packages/protocol/src/domains/chains/utils/chainsUtils.ts
@@ -1,5 +1,8 @@
 import { ChainID } from 'domains/chains/types';
 
+export const getPublicUrl = (url: string) =>
+  url.substring(0, url.lastIndexOf('/'));
+
 const SUBNETS_CHAINS_LIST = [ChainID.SECRET, ChainID.AVALANCHE];
 
 export const checkAvalancheOrSecretAndGetChainId = (chainId: ChainID) => {
