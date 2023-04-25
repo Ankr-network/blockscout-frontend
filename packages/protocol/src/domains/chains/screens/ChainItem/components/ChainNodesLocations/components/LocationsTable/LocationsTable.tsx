716fdb7e631d2ad2082a6448b2e63d278934ee70
Denise <41983880+Denise54321@users.noreply.github.com>
2023-04-25 15:25:35 +0800

    feat(protocol): MRPC-2749 Change Node Location View (#3350)

--- packages/protocol/src/domains/chains/screens/ChainItem/components/ChainNodesLocations/components/LocationsTable/LocationsTable.tsx
+++ packages/protocol/src/domains/chains/screens/ChainItem/components/ChainNodesLocations/components/LocationsTable/LocationsTable.tsx
@@ -1,32 +1,43 @@
-import { useMemo } from 'react';
-import { Paper, TableContainer } from '@mui/material';
 import { t } from '@ankr.com/common';
+import { Typography } from '@mui/material';
 
-import { VirtualTable } from 'uiKit/VirtualTable';
 import { LocationsTableProps } from './LocationsTableProps';
-import { getRows, useColumns } from './LocationsTableUtils';
 import { useLocationsTableStyles } from './useLocationsTableStyles';
-import { TABLE_WIDTH } from '../../useChainNodesLocationsStyles';
 
-export const LocationsTable = ({
-  loading,
-  nodesDetail,
-}: LocationsTableProps) => {
+export const LocationsTable = ({ nodesRows }: LocationsTableProps) => {
   const { classes } = useLocationsTableStyles();
-  const columns = useColumns();
-
-  const rows = useMemo(() => getRows(nodesDetail), [nodesDetail]);
 
   return (
-    <TableContainer component={Paper} className={classes.root} elevation={0}>
-      <VirtualTable
-        initializing={loading}
-        cols={columns}
-        rows={rows}
-        classes={{ container: classes.tableContainer, head: classes.tableHead }}
-        minWidth={TABLE_WIDTH}
-        emptyMessage={t('chain-item.locations.empty')}
-      />
-    </TableContainer>
+    <div className={classes.root}>
+      <Typography className={classes.title}>
+        {t('chain-item.locations.head.location')}
+      </Typography>
+      {nodesRows.map(item => (
+        <div className={classes.row} key={item.continent}>
+          <Typography className={classes.continent} variant="body2">
+            {item.continent}
+          </Typography>
+          <div className={classes.indicate}>
+            {!item.isPremium && (
+              <div
+                className={classes.free}
+                style={{ width: `${item.freePercent}` }}
+              />
+            )}
+            <div className={classes.premiumItem}>
+              <div
+                className={classes.premium}
+                style={{ width: `${item.premiumPercent}` }}
+              />
+              {!item.isPremium && (
+                <Typography className={classes.count}>
+                  {t('chain-item.locations.head.count')}
+                </Typography>
+              )}
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
   );
 };
