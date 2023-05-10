17cfeeb7a1fc20104f762fe393b78847338862f6
Dmitriy Lisyanskiy <baomastr@users.noreply.github.com>
2023-05-10 18:16:23 +0400

    fix(protocol): MRPC-2868 balances refetch on reload (#3403)
    * fix(protocol): MRPC-2868 balances refetch on reload
    
    * fix(protocol): MRPC-2868 group data on page reload
    
    * fix(protocol): MRPC-2868 add condition for dev role

--- packages/protocol/src/domains/account/hooks/useBalance.ts
+++ packages/protocol/src/domains/account/hooks/useBalance.ts
@@ -1,44 +1,18 @@
-import { useEffect } from 'react';
-
 import { Balance as AccountBalance } from 'domains/account/actions/balance/types';
-import { Options, useQueryEndpoint } from 'hooks/useQueryEndpoint';
+import { useQueryEndpoint } from 'hooks/useQueryEndpoint';
 import { accountFetchBalance } from 'domains/account/actions/balance/fetchBalance';
 import { defaultBalance } from 'domains/account/actions/balance/const';
-import { useAuth } from 'domains/auth/hooks/useAuth';
-import { useSelectedUserGroup } from 'domains/userGroup/hooks/useSelectedUserGroup';
 
 export interface Balance extends AccountBalance {
   isLoading: boolean;
   isLoadingInitially: boolean;
 }
 
-const options: Options = {
-  subscriptionOptions: {
-    pollingInterval: 30_000,
-  },
-};
-
 export const useBalance = (): Balance => {
-  const { isLoggedIn } = useAuth();
-
-  const [fetchBalance, { data: balances, isLoading }] = useQueryEndpoint(
-    accountFetchBalance,
-    options,
-  );
+  const [, { data: balances, isLoading }] =
+    useQueryEndpoint(accountFetchBalance);
 
   const isLoadingInitially = !balances && isLoading;
-
-  const { selectedGroupAddress: group } = useSelectedUserGroup();
-
-  useEffect(() => {
-    if (isLoggedIn) {
-      const { unsubscribe } = fetchBalance({ group });
-
-      return unsubscribe;
-    }
-
-    return () => {};
-  }, [fetchBalance, isLoggedIn, group]);
 
   return {
     ...(balances || defaultBalance),
