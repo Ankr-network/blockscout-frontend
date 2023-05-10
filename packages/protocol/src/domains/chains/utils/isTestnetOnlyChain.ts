0339b99cd46631bc7158b1e081402b08aca525ab
Denise <41983880+Denise54321@users.noreply.github.com>
2023-05-10 19:52:45 +0800

    feat(protocol): MRPC-2832 Add Base Chain (#3394)

--- packages/protocol/src/domains/chains/utils/isTestnetOnlyChain.ts
+++ packages/protocol/src/domains/chains/utils/isTestnetOnlyChain.ts
@@ -1,7 +1,7 @@
 import { ChainID } from 'domains/chains/types';
 
-const { MANTLE, ROLLUX, ZETACHAIN, SCROLL } = ChainID;
-const testnetOnlyChains: ChainID[] = [MANTLE, ROLLUX, ZETACHAIN, SCROLL];
+const { MANTLE, ROLLUX, ZETACHAIN, BASE, SCROLL } = ChainID;
+const testnetOnlyChains: ChainID[] = [MANTLE, ROLLUX, ZETACHAIN, BASE, SCROLL];
 
 export const isTestnetOnlyChain = (chainID: ChainID) =>
   testnetOnlyChains.includes(chainID);
