72d8f16b202552f45344ad0aab0f6d668c68f0e0
Dmitry Kovtunov <kovtunov@ankr.com>
2023-05-14 18:16:02 +0300

    fix(mrpc): MRPC-2802 Fixed layout reloading after project selecting (#3414)

--- packages/protocol/src/domains/chains/hooks/useUserEndpointToken.ts
+++ packages/protocol/src/domains/chains/hooks/useUserEndpointToken.ts
@@ -1,14 +1,24 @@
-import { useJwtTokenManager } from 'domains/jwtToken/hooks/useJwtTokenManager';
+import {
+  IUserJwtToken,
+  fetchAllJwtTokenRequests,
+} from 'domains/jwtToken/action/getAllJwtToken';
 import { useAuth } from 'domains/auth/hooks/useAuth';
 import { PRIMARY_TOKEN_INDEX } from 'domains/jwtToken/utils/utils';
 import { useTokenManagerConfigSelector } from 'domains/jwtToken/hooks/useTokenManagerConfigSelector';
 import { useGroupJwtToken } from 'domains/userGroup/hooks/useGroupJwtToken';
 import { useSelectedUserGroup } from 'domains/userGroup/hooks/useSelectedUserGroup';
+import { useQueryEndpoint } from 'hooks/useQueryEndpoint';
+
+const defaultData: IUserJwtToken = {
+  jwtTokens: [],
+};
 
 export const useUserEndpointToken = () => {
   const { workerTokenData } = useAuth();
   const { tokenIndex } = useTokenManagerConfigSelector();
-  const { jwtTokens, isLoading } = useJwtTokenManager();
+  const [, { data: { jwtTokens } = defaultData, isLoading }] = useQueryEndpoint(
+    fetchAllJwtTokenRequests,
+  );
   const { groupToken } = useGroupJwtToken();
   const { isGroupSelected } = useSelectedUserGroup();
 
