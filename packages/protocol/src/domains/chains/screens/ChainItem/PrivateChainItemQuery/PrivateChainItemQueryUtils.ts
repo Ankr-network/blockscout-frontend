f8fc5a5eadaa74b6b4793d35763b0cc9bb5686e0
Dmitry Kovtunov <kovtunov@ankr.com>
2023-05-14 18:16:02 +0300

    fix(mrpc): MRPC-2802 Fixed layout reloading after project selecting (#3414)

--- packages/protocol/src/domains/chains/screens/ChainItem/PrivateChainItemQuery/PrivateChainItemQueryUtils.ts
+++ packages/protocol/src/domains/chains/screens/ChainItem/PrivateChainItemQuery/PrivateChainItemQueryUtils.ts
@@ -8,16 +8,15 @@ import { useQueryEndpoint } from 'hooks/useQueryEndpoint';
 export const usePrivateChainItemQuery = (chainId: string) => {
   const userEndpointToken = useUserEndpointToken();
 
-  const [fetchPrivateChain, fetchChainState, reset] = useQueryEndpoint(
+  const [fetchPrivateChain, fetchChainState] = useQueryEndpoint(
     chainsFetchPrivateChain,
   );
 
   useEffect(() => {
     if (userEndpointToken) {
-      reset();
       fetchPrivateChain({ chainId: chainId as ChainID, userEndpointToken });
     }
-  }, [reset, chainId, userEndpointToken, fetchPrivateChain]);
+  }, [chainId, userEndpointToken, fetchPrivateChain]);
 
   return fetchChainState;
 };
