c5d0ca161c0681a9bd7dfe7f35fbae65ebe01bf4
Denise <41983880+Denise54321@users.noreply.github.com>
2023-05-17 22:36:53 +0800

    feat(protocol): MRPC-2902 Update Text (#3440)
    * feat(protocol): MRPC-2902 Update Text
    
    * feat(protocol): MRPC-2902 Update Text

--- packages/protocol/src/domains/chains/screens/ChainItem/components/GetStartedSection/components/MainEndpoints/utils/getTitle.ts
+++ packages/protocol/src/domains/chains/screens/ChainItem/components/GetStartedSection/components/MainEndpoints/utils/getTitle.ts
@@ -1,14 +1,9 @@
 import { text } from './text';
 
 export interface TitleParams {
-  chainName: string;
   isMultiChain: boolean;
   urlsCount: number;
 }
 
-export const getTitle = ({
-  chainName,
-  isMultiChain,
-  urlsCount: urls,
-}: TitleParams) =>
-  isMultiChain ? text('title-multichain') : text('title', { chainName, urls });
+export const getTitle = ({ isMultiChain, urlsCount: urls }: TitleParams) =>
+  isMultiChain ? text('title-multichain') : text('title', { urls });
