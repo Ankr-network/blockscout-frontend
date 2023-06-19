805450a83a98be7902690e1470e96a6a2ed236fb
evgeniy-bond <evgeniy.bondarev.01@gmail.com>
2023-06-19 14:33:16 +0300

    feat(mrpc): MRPC-3143 fix gauth login (#3611)

--- packages/protocol/src/domains/auth/actions/connect/connectInitiator.ts
+++ packages/protocol/src/domains/auth/actions/connect/connectInitiator.ts
@@ -5,7 +5,7 @@ import { IAuthSlice } from 'domains/auth/store/authSlice';
 
 // authConnectInitiator - authConnect - authMakeAuthorization
 // 1) call authConnectInitiator
-// 2) listenerMiddleware will catch oauthLoginInitiator action
+// 2) listenerMiddleware will catch authConnectInitiator action
 // 3) effect will initiate authConnect, add authorization token and ask user's 2fa status
 // 4) if user enabled 2fa, 2fa dialog will be opened
 // 5) effect will launch authMakeAuthorization with 2fa code
