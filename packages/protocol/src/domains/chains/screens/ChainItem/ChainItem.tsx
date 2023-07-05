33edd2b5f3d33a513cfa6e11c1819e8bbd8bcb29
evgeniy-bond <evgeniy.bondarev.01@gmail.com>
2023-07-05 16:21:07 +0300

    feat(mrpc): MRPC-3256 Add banner for zkSync era page (#3678)
    * feat(mrpc): MRPC-3256 Add banner for zkSync era page
    
    * feat(mrpc): MRPC-3256 update CHAIN_IDS_BY_USAGE

--- packages/protocol/src/domains/chains/screens/ChainItem/ChainItem.tsx
+++ packages/protocol/src/domains/chains/screens/ChainItem/ChainItem.tsx
@@ -1,8 +1,12 @@
+import { tHTML } from '@ankr.com/common';
+
+import { ChainID } from 'domains/chains/types';
 import { ChainsRoutesConfig } from 'domains/chains/routes';
 import { NoReactSnap } from 'uiKit/NoReactSnap';
 import { useAuth } from 'domains/auth/hooks/useAuth';
 import { PrivateChainItemQuery } from './PrivateChainItemQuery';
 import { PublicChainItemQuery } from './PublicChainItemQuery';
+import { ChainItemBanner } from './components/ChainItemBanner';
 
 export const ChainItem = () => {
   const { chainId } = ChainsRoutesConfig.chainDetails.useParams();
@@ -11,6 +15,9 @@ export const ChainItem = () => {
 
   return (
     <NoReactSnap>
+      {chainId === ChainID.ZKSYNC_ERA && (
+        <ChainItemBanner message={tHTML('chain-item.banner-zksync_era')} />
+      )}
       {hasPrivateAccess ? (
         <PrivateChainItemQuery chainId={chainId} />
       ) : (
