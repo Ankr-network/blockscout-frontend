3c4d1bc62eca604af213ded96d3e1ab75219dbf5
evgeniy-bond <evgeniy.bondarev.01@gmail.com>
2023-07-05 16:21:07 +0300

    feat(mrpc): MRPC-3256 Add banner for zkSync era page (#3678)
    * feat(mrpc): MRPC-3256 Add banner for zkSync era page
    
    * feat(mrpc): MRPC-3256 update CHAIN_IDS_BY_USAGE

---
+++ packages/protocol/src/domains/chains/screens/ChainItem/components/ChainItemBanner/ChainItemBanner.tsx
@@ -0,0 +1,21 @@
+import { Paper } from '@mui/material';
+import { Mark } from '@ankr.com/ui';
+
+import { InfoBanner } from 'modules/common/components/InfoBanner';
+import { useChainItemBannerStyles } from './ChainItemBannerStyles';
+
+interface ChainItemBannerProps {
+  message: string;
+}
+
+// Don't remove. Use this banner for info message
+
+export const ChainItemBanner = ({ message }: ChainItemBannerProps) => {
+  const { classes } = useChainItemBannerStyles();
+
+  return (
+    <Paper className={classes.root}>
+      <InfoBanner icon={<Mark />} message={message} />
+    </Paper>
+  );
+};
