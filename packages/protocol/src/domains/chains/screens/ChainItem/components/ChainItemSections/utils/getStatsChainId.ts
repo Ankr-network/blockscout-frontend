0b37899e1fd85878539ae2e64dcf6184e03aedd7
Dmitriy Lisyanskiy <baomastr@users.noreply.github.com>
2023-08-08 09:26:09 +0200

    fix(protocol): withExceptions for getStatsChainId (#3860)

--- packages/protocol/src/domains/chains/screens/ChainItem/components/ChainItemSections/utils/getStatsChainId.ts
+++ packages/protocol/src/domains/chains/screens/ChainItem/components/ChainItemSections/utils/getStatsChainId.ts
@@ -28,7 +28,6 @@ export const getStatsChainId = ({
   isChainProtocolSwitchEnabled,
   publicChain,
   chainProtocol,
-  withExceptions,
 }: StatsChainIdParams) => {
   const keepEVMChainID =
     publicChain.id === ChainID.ZETACHAIN || publicChain.id === ChainID.HORIZEN;
