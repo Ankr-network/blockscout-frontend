64354d6ccc530d51d33054d0482264c08bc9f54a
evgeniy-bond <evgeniy.bondarev.01@gmail.com>
2023-08-17 11:28:49 +0300

    fix: MRPC-3389 fix jwt manager skeleton styles (#3883)

--- packages/protocol/src/domains/jwtToken/components/JwtTokenManager/useJwtTokenManagerStyles.ts
+++ packages/protocol/src/domains/jwtToken/components/JwtTokenManager/useJwtTokenManagerStyles.ts
@@ -3,7 +3,7 @@ import { makeStyles } from 'tss-react/mui';
 export const useJwtTokenManagerStyles = makeStyles()(theme => ({
   root: {
     marginBottom: theme.spacing(7.5),
-    height: theme.spacing(24),
+    height: 98,
   },
   skeleton: {
     display: 'flex',
@@ -12,8 +12,8 @@ export const useJwtTokenManagerStyles = makeStyles()(theme => ({
   },
   item: {
     flexShrink: 0,
-    width: theme.spacing(51),
-    height: theme.spacing(21),
+    width: 204,
+    height: 84,
     borderRadius: 20,
     backgroundColor: theme.palette.background.paper,
   },
