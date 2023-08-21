e5d481002712841729572e02e8fe6bcc8d091fa1
evgeniy-bond <evgeniy.bondarev.01@gmail.com>
2023-08-21 14:51:51 +0300

    fix: MRPC-3562 remove failed requests banner (#3922)

--- packages/protocol/src/domains/chains/screens/Chains/components/PrivateChains/PrivateChainsTop/PrivateChainsTop.tsx
+++ packages/protocol/src/domains/chains/screens/Chains/components/PrivateChains/PrivateChainsTop/PrivateChainsTop.tsx
@@ -1,7 +1,6 @@
 import { useAuth } from 'domains/auth/hooks/useAuth';
 import { ExpiredTokenBanner } from 'domains/auth/components/ExpiredTokenBanner';
 import { Timeframe } from 'domains/chains/types';
-import { FailedRequestsBannerContainer } from 'domains/chains/components/FailedRequestsBanner/FailedRequestsBannerContainer';
 import { RequestsBannerContainer } from 'domains/chains/components/RequestsBannerContainer/RequestsBannerContainer';
 import { JwtTokenManager } from 'domains/jwtToken/components/JwtTokenManager';
 import { GuardUserGroup } from 'domains/userGroup/components/GuardUserGroup';
@@ -16,18 +15,12 @@ interface IPrivateChainsProps {
 }
 
 export const PrivateChainsTop = ({ timeframe }: IPrivateChainsProps) => {
-  const { isFreePremium, hasUserEndpointToken, hasPremium } = useAuth();
+  const { hasUserEndpointToken, hasPremium } = useAuth();
   const { isOpened, onClose, type } = useURLBasedUpgradePlanDialog();
 
   return (
     <>
       <ExpiredTokenBanner />
-      {isFreePremium && hasUserEndpointToken && (
-        <GuardUserGroup blockName={BlockWithPermission.UsageData}>
-          <FailedRequestsBannerContainer timeframe={Timeframe.Month} />
-        </GuardUserGroup>
-      )}
-
       {hasPremium && hasUserEndpointToken && (
         <GuardUserGroup blockName={BlockWithPermission.UsageData}>
           <RequestsBannerContainer timeframe={timeframe} />
