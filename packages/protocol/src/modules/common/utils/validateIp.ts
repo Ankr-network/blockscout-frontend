8380f681eed2b9580fd7916ffb1aa9908602c2b3
t.polozov <39823227+PolozovT@users.noreply.github.com>
2023-08-24 14:21:23 +0300

    feat: MRPC-3497 added ip v6 validation (#3878)
    * feat: MRPC-3497 added ip v6 validation
    
    * feat: MRPC-3497 added link to ip v6 regex
    
    * feat: MRPC-3497 updated regex
    
    * feat: MRPC-3497 updated regex for ip v4, v6

--- packages/protocol/src/modules/common/utils/validateIp.ts
+++ packages/protocol/src/modules/common/utils/validateIp.ts
@@ -1,13 +1,14 @@
 import { t } from '@ankr.com/common';
 
 import { isIpAddress } from './isIpAddress';
+import { isIpv6Address } from './isIpv6Address';
 
 export const validateIp = (value?: string) => {
   if (typeof value !== 'string') {
     return t('validation.required');
   }
 
-  if (!isIpAddress(value)) {
+  if (!isIpAddress(value) && !isIpv6Address(value)) {
     return t('validation.ip-validation');
   }
 
