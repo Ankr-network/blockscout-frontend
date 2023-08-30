198bcc59e5a73f71e2fa986e432a5564fb4cb675
t.polozov <39823227+PolozovT@users.noreply.github.com>
2023-08-30 14:20:37 +0300

    feat: MRPC-3603 added library to validate ipv6 (#3960)

--- packages/protocol/src/modules/common/utils/isIpv6Address.ts
+++
@@ -1,6 +0,0 @@
-export function isIpv6Address(str: string): boolean {
-  // https://regex101.com/library/aL7tV3?orderBy=RELEVANCE&search=ip
-  return /(?!^(?:(?:.*(?:::.*::|:::).*)|::|[0:]+[01]|.*[^:]:|[0-9a-fA-F](?:.*:.*){8}[0-9a-fA-F]|(?:[0-9a-fA-F]:){1,6}[0-9a-fA-F])$)^(?:(::|[0-9a-fA-F]{1,4}:{1,2})([0-9a-fA-F]{1,4}:{1,2}){0,6}([0-9a-fA-F]{1,4}|::)?)$/.test(
-    str,
-  );
-}
