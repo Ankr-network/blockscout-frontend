0f3e8f88f5085655c89a5b75d0190020cefed479
Dmitriy Lisyanskiy <baomastr@users.noreply.github.com>
2023-12-13 16:49:53 +0530

    fix(protocol): MRPC-4023 fix collapsing string state calculating (#4254)

--- packages/protocol/src/modules/common/components/CollapsingString/useCollapsingStringStyles.ts
+++ packages/protocol/src/modules/common/components/CollapsingString/useCollapsingStringStyles.ts
@@ -1,43 +1,40 @@
 import { makeStyles } from 'tss-react/mui';
 
-export const useCollapsingStringStyles = makeStyles<{ maxWidth: number }>()(
-  (theme, { maxWidth }) => ({
-    root: {
-      display: 'flex',
-      alignItems: 'center',
-      maxWidth: '100%',
-    },
-    rootOpened: {
-      flexDirection: 'column',
-      alignItems: 'flex-start',
-    },
-    wrapper: {
-      display: 'inline-flex',
-    },
-    wrapperCollapsed: {
-      overflow: 'hidden',
-      textOverflow: 'ellipsis',
-      maxWidth,
-    },
-    wrapperOpened: {
-      flexDirection: 'column',
-    },
-    text: {
-      wordBreak: 'break-word',
-      fontSize: 14,
-    },
-    btn: {
-      display: 'inline-flex',
-      cursor: 'pointer',
-      fontSize: 14,
+export const useCollapsingStringStyles = makeStyles()(theme => ({
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+    maxWidth: '100%',
+  },
+  rootOpened: {
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+  },
+  wrapper: {
+    display: 'inline-flex',
+  },
+  wrapperCollapsed: {
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+  },
+  wrapperOpened: {
+    flexDirection: 'column',
+  },
+  text: {
+    wordBreak: 'break-word',
+    fontSize: 14,
+  },
+  btn: {
+    display: 'inline-flex',
+    cursor: 'pointer',
+    fontSize: 14,
 
-      '&:hover': {
-        color: theme.palette.primary.dark,
-      },
+    '&:hover': {
+      color: theme.palette.primary.dark,
     },
-    btnMore: {
-      marginLeft: theme.spacing(1),
-      fontSize: 14,
-    },
-  }),
-);
+  },
+  btnMore: {
+    marginLeft: theme.spacing(1),
+    fontSize: 14,
+  },
+}));
