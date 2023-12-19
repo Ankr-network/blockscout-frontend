298a9abbb773de9aa23646937941d6958b456670
t.polozov <polozovt@yandex.ru>
2023-12-19 11:45:15 +0300

    fix: MRPC-4177 change typo, updated window replace func

--- packages/admin-mrpc/src/modules/clients/actions/fetchExternalEmail.ts
+++ packages/admin-mrpc/src/modules/clients/actions/fetchExternalEmail.ts
@@ -30,6 +30,10 @@ export const {
                 email: res.data,
               }),
             );
+            const clearUrl = window.location.origin + window.location.pathname;
+
+            // Clear query params
+            window.location.replace(clearUrl);
           }
         });
       },
