cadca24e568c590ab438e8d303796421effa3cf4
Dmitriy Lisyanskiy <baomastr@users.noreply.github.com>
2024-02-07 20:12:53 +0530

    fix(protocol): MRPC-4327, MRPC-4328 notifications (#4436)
    * fix(protocol): MRPC-4327, MRPC-4328 notifications
    
    * fix(protocol): MRPC-4327 add team name to notification

--- packages/protocol/src/domains/userSettings/actions/teams/leaveTeam.ts
+++ packages/protocol/src/domains/userSettings/actions/teams/leaveTeam.ts
@@ -5,6 +5,8 @@ import { MultiService } from 'modules/api/MultiService';
 import { RequestType, web3Api } from 'store/queries';
 import { createNotifyingQueryFn } from 'store/utils/createNotifyingQueryFn';
 import { NotificationActions } from 'domains/notification/store/NotificationActions';
+import { selectUserGroups } from 'domains/userGroup/store';
+import { RootState } from 'store';
 
 export const {
   endpoints: { leaveTeam },
@@ -23,15 +25,35 @@ export const {
 
         return { data };
       }),
-      onQueryStarted: async (_args, { dispatch, queryFulfilled }) => {
-        await queryFulfilled;
-
-        dispatch(
-          NotificationActions.showNotification({
-            message: t('common.success-message'),
-            severity: 'success',
-          }),
+      onQueryStarted: async (
+        { group = '' },
+        { dispatch, queryFulfilled, getState },
+      ) => {
+        const userGroups = selectUserGroups(getState() as RootState);
+        const currentGroup = userGroups.find(
+          userGroup => userGroup.address.toLowerCase() === group.toLowerCase(),
         );
+        const currentGroupName = currentGroup?.name;
+
+        const response = await queryFulfilled;
+
+        if (response?.data?.result) {
+          dispatch(
+            NotificationActions.showNotification({
+              message: t('teams.leave-team.success-message', {
+                teamName: currentGroupName,
+              }),
+              severity: 'success',
+            }),
+          );
+        } else {
+          dispatch(
+            NotificationActions.showNotification({
+              message: t('common.error-message'),
+              severity: 'error',
+            }),
+          );
+        }
       },
     }),
   }),
