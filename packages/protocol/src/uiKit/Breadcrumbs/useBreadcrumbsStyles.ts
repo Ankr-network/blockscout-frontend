88eaa148d448493be9def907ae9519dd47045d92
t.polozov <39823227+PolozovT@users.noreply.github.com>
2024-02-12 19:33:34 +0300

    bugfix: MRPC-4277 select content moving (#4448)
    * feat: MRPC-4277 updated breadcrumbs styles

--- packages/protocol/src/uiKit/Breadcrumbs/useBreadcrumbsStyles.ts
+++ packages/protocol/src/uiKit/Breadcrumbs/useBreadcrumbsStyles.ts
@@ -4,42 +4,46 @@ export const useBreadcrumbsStyles = makeStyles<boolean>()(
   (theme, shouldShowMobileBreadcrumbs) => ({
     link: shouldShowMobileBreadcrumbs
       ? {
-          cursor: 'pointer',
-          color: theme.palette.grey[400],
-          '&.custom-link:hover': {
-            textDecoration: 'none',
+        cursor: 'pointer',
+        color: theme.palette.grey[400],
+        '&.custom-link:hover': {
+          textDecoration: 'none',
 
-            color: theme.palette.primary.main,
-          },
-          /* just icon "back" for breadcrumbs link */
+          color: theme.palette.primary.main,
+        },
+        /* just icon "back" for breadcrumbs link */
 
+        padding: 10,
+        width: 36,
+        height: 36,
+        display: 'inline-flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }
+      : {
+        cursor: 'pointer',
+        color: theme.palette.grey[900],
+
+        display: 'flex',
+        height: 22,
+
+        '&.custom-link:hover': {
+          textDecoration: 'none',
+
+          color: theme.palette.primary.main,
+        },
+        /* just icon "back" for breadcrumbs link */
+        [theme.breakpoints.down('md')]: {
           padding: 10,
+          border: `1px solid ${theme.palette.grey[500]}`,
+          borderRadius: '50%',
           width: 36,
           height: 36,
           display: 'inline-flex',
           justifyContent: 'center',
           alignItems: 'center',
-        }
-      : {
-          cursor: 'pointer',
-          color: theme.palette.grey[900],
-          '&.custom-link:hover': {
-            textDecoration: 'none',
-
-            color: theme.palette.primary.main,
-          },
-          /* just icon "back" for breadcrumbs link */
-          [theme.breakpoints.down('md')]: {
-            padding: 10,
-            border: `1px solid ${theme.palette.grey[500]}`,
-            borderRadius: '50%',
-            width: 36,
-            height: 36,
-            display: 'inline-flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-          },
         },
+      },
 
     separator: {
       color: theme.palette.grey[400],
@@ -62,8 +66,8 @@ export const useBreadcrumbsStyles = makeStyles<boolean>()(
     },
     muiSeparator: shouldShowMobileBreadcrumbs
       ? {
-          margin: 0,
-        }
+        margin: 0,
+      }
       : {},
     breadcrumbsRoot: {
       overflow: 'hidden',
