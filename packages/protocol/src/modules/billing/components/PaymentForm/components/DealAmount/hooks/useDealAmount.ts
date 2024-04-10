292f75152301acaa256a83b7ceca889dcbb5aa4b
Dmitriy Lisyanskiy <dmitriy.lisyanskiy@ankr.com>
2024-04-10 18:06:56 +0400

    fix(protocol): remove bundles refetching

--- packages/protocol/src/modules/billing/components/PaymentForm/components/DealAmount/hooks/useDealAmount.ts
+++ packages/protocol/src/modules/billing/components/PaymentForm/components/DealAmount/hooks/useDealAmount.ts
@@ -1,9 +1,8 @@
 import { t } from '@ankr.com/common';
-import { useEffect, useMemo } from 'react';
+import { useMemo } from 'react';
 
 import { ECurrency, IAmount } from 'modules/billing/types';
 import { useBundlePaymentPlans } from 'domains/account/hooks/useBundlePaymentPlans';
-import { useLazyFetchBundlePaymentPlansQuery } from 'domains/account/actions/bundles/fetchBundlePaymentPlans';
 
 import { IDealAmountProps } from '../DealAmount';
 
@@ -13,14 +12,6 @@ export const useDealAmount = (): IDealAmountProps => {
   const { deal500: deal500Plan, loading: isLoading } = useBundlePaymentPlans({
     skipFetching: true,
   });
-
-  const [fetchBundles] = useLazyFetchBundlePaymentPlansQuery();
-
-  useEffect(() => {
-    if (!isLoading && !deal500Plan) {
-      fetchBundles();
-    }
-  }, [deal500Plan, isLoading, fetchBundles]);
 
   const title = deal500Plan?.bundle.name ?? t(titleFallbackKey);
 
