38b3f9e7d715a733d14695539ee57af5fa54c954
Dmitriy Lisyanskiy <baomastr@users.noreply.github.com>
2024-06-11 13:52:17 +0400

    feat(protocol): MRPC-4928 remove stats requests from v2 (#4723)
    * feat(protocol): MRPC-4897 add usage endpoint and typings
    
    * feat(protocol): MRPC-4897 add usage request example to comment
    
    * feat(protocol): MRPC-4897 remove some unuseful code
    
    * feat(protocol): MRPC-4905 add v2 layout for all chains view
    
    * feat(protocol): MRPC-4905 fix some namings and typings
    
    * feat(protocol): MRPC-4905 use miliseconds const
    
    * feat(protocol): MRPC-4906 add filters by blockchain
    
    * feat(protocol): MRPC-4906 add filters by apiKey
    
    * feat(protocol): MRPC-4906 remove nocheck for usePrivateChainsData
    
    * feat(protocol): MRPC-4906 chore: move selectors to separate component
    
    * feat(protocol): MRPC-4906 add grop param to requests, removed unused requests for enterprise users
    
    * feat(protocol): MRPC-4906 add wrappers for v1/v2 widgets
    
    * feat(protocol): MRPC-4906 chore selectors content
    
    * feat(protocol): MRPC-4906 chore error handling
    
    * feat(protocol): MRPC-4906 add endpoints mask for projects widget
    
    * feat(protocol): MRPC-4906 add regions mapping
    
    * feat(protocol): MRPC-4906 fix response status widget content
    
    * feat(protocol): MRPC-4906 add mapping for blockchains
    
    * feat(protocol): MRPC-4906 fix chain id for requests
    
    * feat(protocol): MRPC-4906 add chain calls squeeze for dashboard widget
    
    * feat(protocol): MRPC-4906 fix options deps
    
    * feat(protocol): MRPC-4906 fix block height
    
    * feat(protocol): MRPC-4906 fix loading state
    
    * feat(protocol): MRPC-4906 fix loading state
    
    * feat(protocol): MRPC-4906 fix redirect on start building btn
    
    * feat(protocol): MRPC-4928 remove stats requests from v2
    
    * feat(protocol): MRPC-4928 remove stats requests from v2. should be added filtering
    
    * feat(web3api): MRPC-4928 add blockchains for enterprise to store
    
    * feat(web3api): MRPC-4928 map response codes
    
    * feat(web3api): MRPC-4928 remove unused code
    
    * feat(web3api): MRPC-4928 fix error
    
    * feat(web3api): MRPC-4928 fix error
    
    * feat(web3api): MRPC-4928 move mapper to utils
    
    * feat(web3api): MRPC-4928 add comment in action
    
    * feat(web3api): MRPC-4928 remove response codes mapping
    
    * feat(web3api): MRPC-4928 fix others count
    
    * feat(web3api): MRPC-4928 fix others condition

--- packages/protocol/src/domains/dashboard/store/utils/squeezeChainCallsV2.ts
+++ packages/protocol/src/domains/dashboard/store/utils/squeezeChainCallsV2.ts
@@ -8,14 +8,19 @@ export const squeezeChainCalls = (
   elements: PieChartData[] | null = [],
 ): PieChartData[] => {
   const firstItems = elements?.slice(0, MAX_PIECES - 1);
-  const others = elements?.slice(MAX_PIECES - 1);
 
   if (!firstItems) {
     return [];
   }
 
+  const others = elements?.slice(MAX_PIECES - 1);
+
+  if (!others?.length) {
+    return firstItems;
+  }
+
   const lastItem: PieChartData = {
-    value: others?.reduce((sum, item) => sum + Number(item.value), 0) || 0,
+    value: others?.reduce((sum, item) => sum + Number(item.value), 0),
     name: text('others'),
   };
 
