9caf6ccf381057f25f2b4597d0a29ee422102642
Dmitry Kovtunov <kovtunov@ankr.com>
2024-08-23 15:09:26 +0300

    feat(protocol): MRPC-4992 Implemented Copy Referral Link button

---
+++ packages/protocol/src/domains/referral/screens/ReferralPage/components/CopyReferralLinkButton/CopyReferralLinkButton.tsx
@@ -0,0 +1,36 @@
+import CopyToClipboard from 'react-copy-to-clipboard';
+import { Button } from '@mui/material';
+import { Copy } from '@ankr.com/ui';
+
+import { TextTooltip } from 'modules/common/components/TextTooltip';
+import { useCopyButton } from 'uiKit/CopyButton/hooks/useCopyButton';
+import { useTranslation } from 'modules/i18n/hooks/useTranslation';
+
+import { copyReferralLinkButtonTranslation } from './translation';
+import { useCopyReferralLinkButtonStyles } from './useCopyReferralLinkButtonStyles';
+import { useReferralLink } from './hooks/useReferralLink';
+
+export const CopyReferralLinkButton = () => {
+  const { onCopy, tooltipProps } = useCopyButton();
+
+  const { link } = useReferralLink();
+
+  const { classes } = useCopyReferralLinkButtonStyles();
+  const { keys, t } = useTranslation(copyReferralLinkButtonTranslation);
+
+  return (
+    <CopyToClipboard onCopy={onCopy} text={link}>
+      <TextTooltip {...tooltipProps} title={t(keys.copied)}>
+        <Button
+          className={classes.root}
+          color="primary"
+          size="small"
+          startIcon={<Copy size={20} />}
+          variant="outlined"
+        >
+          {t(keys.text)}
+        </Button>
+      </TextTooltip>
+    </CopyToClipboard>
+  );
+};
