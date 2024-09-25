5408a0d86824a11105cef6a8a24249e47ae3e8f1
Dmitry Kovtunov <kovtunov@ankr.com>
2024-09-25 15:25:03 +0300

    feat(protocol): MRPC-5386 Deleted B2B referral programs (#4905)

--- packages/protocol/src/modules/referralProgram/const.ts
+++ packages/protocol/src/modules/referralProgram/const.ts
@@ -1,5 +1,3 @@
-import xaiBanner from 'modules/referralProgram/assets/xai-banner.png';
-
 import { IB2BReferralProgram } from './types';
 
 export enum ApplyReferralCodeErrorCode {
@@ -12,16 +10,5 @@ export const MIN_REFERRAL_CODE_LENGTH = 3;
 export const MAX_REFERRAL_CODE_LENGTH = 32;
 
 export const B2B_REFERRAL_PROGRAMS: IB2BReferralProgram[] = [
-  {
-    banner: xaiBanner,
-    blockchainName: 'Xai',
-    bundleId: 'e2f9411b-ad39-4a7f-9095-cc55ca7cc8ec',
-    referralCode: '5MhwK32c55',
-  },
-  {
-    banner: xaiBanner,
-    blockchainName: 'Expired',
-    bundleId: 'e2f9411b-ad39-4a7f-9095-cc55ca7cc8ec',
-    referralCode: '7MhwK32c77',
-  },
+  // fill in this array to register a B2B referral program
 ];
