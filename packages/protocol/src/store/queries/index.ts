ecde3fd026960d674aa99c0963e12c4f3e80b87b
Dmitry Kovtunov <kovtunov@ankr.com>
2024-11-07 10:36:56 +0300

    fix(protocol): MRPC-5406 Fixed projects widget

--- packages/protocol/src/store/queries/index.ts
+++ packages/protocol/src/store/queries/index.ts
@@ -66,6 +66,7 @@ const endpointsSerializedByParams = [
   'fetchPrivateTop10Stats',
   'fetchPrivateTotalStats',
   'fetchPrivateTotalStatsByRange',
+  'fetchProjectsStats',
   'fetchPublicRequestsCountStats',
   /* notifications */
   'fetchNotifications',
